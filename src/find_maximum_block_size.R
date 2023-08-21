7c44fe1f63328e17791b72b65832900fd4140ced
pdkengne <sielenou_alain@yahoo.fr>
2023-08-21 18:36:52 +0200

    creation of the file find_maximum_block_size.R

---
+++ src/find_maximum_block_size.R
@@ -0,0 +1,18 @@
+find_maximum_block_size <- function(x, m = 50){
+  # x: vector of observations
+  # m: minimum number of blocks
+  
+  size <- length(x)
+  block_size <- floor(size/m)
+  
+  block_size
+}
+
+# examples
+
+x <- rnorm(n = 1000)
+m <- 40
+
+result <- find_maximum_block_size(x, m)
+
+result
