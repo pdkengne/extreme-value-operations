db8909fc46a5abc511b2cfa351f854c22cbe2c9a
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-08-28 01:55:04 +0200

    creation of the file extract_peaks_over_threshold.R

---
+++ src/extract_peaks_over_threshold.R
@@ -0,0 +1,27 @@
+extract_peaks_over_threshold <- function(x, threshold){
+  # x: vector of observations
+  # threshold: threshold to consider
+  
+  # get the peaks over threshold
+  peak_over_threshold <- x[x > threshold]
+  
+  peak_over_threshold
+}
+
+
+# # example 1
+# 
+# x <- rnorm(n = 100)
+# 
+# results <- extract_peaks_over_threshold(x, threshold = 0)
+# 
+# results
+# 
+# 
+# # example 2
+# 
+# x <- 1:100
+# 
+# results <- extract_peaks_over_threshold(x, threshold = 80)
+# 
+# results
