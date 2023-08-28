6e415dff92650cd385a5bb769b7265682ad855d9
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-08-28 04:29:55 +0200

    creation of the file calculate_gp_pdf.R

--- src/calculate_gp_cdf.R
+++ src/calculate_gp_cdf.R
@@ -1,10 +1,10 @@
 # library(extRemes)
 
-calculate_gp_cdf <- function(x, loc = 0, scale = 1, shape = 0, threshold = 0){
+calculate_gp_cdf <- function(x, scale = 1, shape = 0, threshold = 0){
   # x: vector of observations
-  # loc, scale, shape, threshold: location, scale, shape and threshold parameters of the considered gp distribution
+  # loc, scale, shape, threshold: scale, shape and threshold parameters of the considered gp distribution
   
-  gp_cdf <- extRemes::pevd(x, loc, scale, shape, threshold, type = "GP")
+  gp_cdf <- extRemes::pevd(x, scale, shape, threshold, type = "GP")
   
   gp_cdf
 }
@@ -12,20 +12,13 @@ calculate_gp_cdf <- function(x, loc = 0, scale = 1, shape = 0, threshold = 0){
 
 # example 1
 
-result <- calculate_gp_cdf(x = 2:4, loc = 1, scale = 0.5, shape = 0.8, threshold = 1)
+result <- calculate_gp_cdf(x = 2:4, scale = 0.5, shape = 0.8, threshold = 1)
 
 result
 
 
 # example 2
 
-result <- calculate_gp_cdf(x = 2:4, loc = 1, scale = 0.5, shape = 0, threshold = 1)
-
-result
-
-
-# example 3
-
-result <- calculate_gp_cdf(x = 2:4, loc = 1, scale = 0.5, shape = -0.2, threshold = 1)
+result <- calculate_gp_cdf(x = 2:4, scale = 0.5, shape = 0.8, threshold = 0)
 
 result
---
+++ src/calculate_gp_pdf.R
@@ -0,0 +1,24 @@
+# library(extRemes)
+
+calculate_gp_pdf <- function(x, scale = 1, shape = 0, threshold = 0){
+  # x: vector of observations
+  # loc, scale, shape, threshold: scale, shape and threshold parameters of the considered gp distribution
+  
+  gp_pdf <- extRemes::devd(x, scale, shape, threshold, type = "GP")
+  
+  gp_pdf
+}
+
+
+# example 1
+
+result <- calculate_gp_pdf(x = 2:4, scale = 0.5, shape = 0.8, threshold = 1)
+
+result
+
+
+# example 2
+
+result <- calculate_gp_pdf(x = 2:4, scale = 0.5, shape = 0.8, threshold = 0)
+
+result
