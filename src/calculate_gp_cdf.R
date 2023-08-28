8bae5f80863d949cb6139be6cd1170a8b5f04693
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-08-28 15:14:14 +0200

    update of the file calculate_gp_cdf.R

--- src/calculate_gp_cdf.R
+++ src/calculate_gp_cdf.R
@@ -1,10 +1,10 @@
-# library(extRemes)
+# library(evd)
 
-calculate_gp_cdf <- function(x, scale = 1, shape = 0, threshold = 0){
+calculate_gp_cdf <- function(q, scale = 1, shape = 0, threshold = 0){
   # x: vector of observations
   # scale, shape, threshold: scale, shape and threshold parameters of the considered gp distribution
   
-  gp_cdf <- extRemes::pevd(x, scale, shape, threshold, type = "GP")
+  gp_cdf <- evd::pgpd(q, scale, shape, loc = threshold)
   
   gp_cdf
 }
@@ -12,13 +12,20 @@ calculate_gp_cdf <- function(x, scale = 1, shape = 0, threshold = 0){
 
 # # example 1
 # 
-# result <- calculate_gp_cdf(x = 2:4, scale = 0.5, shape = 0.8, threshold = 1)
+# result <- calculate_gp_cdf(q = 2:4, scale = 0.5, shape = 0.8, threshold = 1)
 # 
 # result
 # 
 # 
 # # example 2
 # 
-# result <- calculate_gp_cdf(x = 2:4, scale = 0.5, shape = 0.8, threshold = 0)
+# result <- calculate_gp_cdf(q = 2:4, scale = 0.5, shape = 0, threshold = 1)
+# 
+# result
+# 
+# 
+# # example 3
+# 
+# result <- calculate_gp_cdf(q = 2:4, scale = 0.5, shape = -0.2, threshold = 1)
 # 
 # result
