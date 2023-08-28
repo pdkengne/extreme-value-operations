6ae92025a0bd33fb42beb037ed4e8e898cc04812
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-08-28 15:25:58 +0200

    update of the file calculate_gp_inverse_cdf.R

--- src/calculate_gp_inverse_cdf.R
+++ src/calculate_gp_inverse_cdf.R
@@ -1,4 +1,4 @@
-# library(extRemes)
+# library(evd)
 
 options(digits = 15)
 
@@ -6,10 +6,11 @@ calculate_gp_inverse_cdf <- function(p, scale = 1, shape = 0, threshold = 0){
   # p: vector of probabilities
   # scale, shape, threshold: scale, shape and threshold parameters of the considered gp distribution
   
-  gp_inverse_cdf <- extRemes::qevd(p, scale, shape, threshold, type = "GP")
+  gp_inverse_cdf <- evd::qgpd(p, scale, shape, loc = threshold)
   
   gp_inverse_cdf
 }
+
 
 
 # # example 1
@@ -25,19 +26,40 @@ calculate_gp_inverse_cdf <- function(p, scale = 1, shape = 0, threshold = 0){
 # 
 # probabilities <- seq(from = 10^(-8), to = 1 -10^(-8), length.out = 20)
 # 
-# result <- calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = 0.8, threshold = 0)
+# result <- calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = 0, threshold = 1)
 # 
 # result
 # 
 # 
 # # example 3
 # 
+# probabilities <- seq(from = 10^(-8), to = 1 -10^(-8), length.out = 20)
+# 
+# result <- calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = -0.2, threshold = 1)
+# 
+# result
+# 
+# 
+# # example 4
+# 
 # source("./src/calculate_gp_cdf.R")
 # 
 # probabilities <- seq(from = 1, to = 9, by = 1)/10
+# probabilities
 # 
-# result <- calculate_gp_cdf(x = calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = 0.8, threshold = 1),
+# result <- calculate_gp_cdf(q = calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = 0.8, threshold = 1),
 #                             scale = 0.5, shape = 0.8, threshold = 1)
 # 
 # result
+# 
+# 
+# # example 5
+# 
+# source("./src/calculate_gp_cdf.R")
+# 
+# probabilities <- seq(from = 1, to = 9, by = 1)/10
 # probabilities
+# 
+# result <- calculate_gp_cdf(q = calculate_gp_inverse_cdf(p = probabilities))
+# 
+# result
