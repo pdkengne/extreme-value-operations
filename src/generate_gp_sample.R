ae7e1fe45c56536620ab2a5f9b91727b247d9818
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-08-28 05:09:09 +0200

    update of the file generate_gp_sample.R

--- src/generate_gp_sample.R
+++ src/generate_gp_sample.R
@@ -8,15 +8,15 @@ generate_gp_sample <- function(n = 1, scale = 1, shape = 0, threshold = 0){
 }
 
 
-# example 1
-
-result <- generate_gp_sample(n = 10, scale = 0.5, shape = 0.8, threshold = 0)
-
-result
-
-
-# example 2
-
-result <- generate_gp_sample(n = 10, scale = 0.5, shape = 0.8, threshold = 1)
-
-result
+# # example 1
+# 
+# result <- generate_gp_sample(n = 10, scale = 0.5, shape = 0.8, threshold = 0)
+# 
+# result
+# 
+# 
+# # example 2
+# 
+# result <- generate_gp_sample(n = 10, scale = 0.5, shape = 0.8, threshold = 1)
+# 
+# result
