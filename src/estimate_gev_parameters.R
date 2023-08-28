c64f92896ccf5dfbb53933a737e39137323a893e
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-08-28 18:21:29 +0200

    creation of the file estimate_gev_parameters.R

---
+++ src/estimate_gev_parameters.R
@@ -0,0 +1,50 @@
+# library(evd)
+
+options(digits = 15)
+
+estimate_gev_parameters <- function(x, nsloc = NULL){
+  # x: vector of observations
+  # nsloc: dataframe of covariates for linear modeling of the location parameter
+  
+  gev_model <- evd::fgev(x, nsloc = nsloc, prob = NULL, std.err = TRUE,
+                        corr = FALSE, method = "BFGS", warn.inf = TRUE)
+  
+  gev_model
+}
+
+
+
+# # example 1
+# 
+# source("./src/generate_gev_sample.R")
+# 
+# x <- generate_gev_sample(n = 1000, loc = 1, scale = 0.5, shape = +0.2)
+# 
+# results<- estimate_gev_parameters(x, nsloc = NULL)
+# 
+# results
+# names(results)
+# 
+# 
+# # example 2
+# 
+# source("./src/generate_gev_sample.R")
+# 
+# x <- generate_gev_sample(n = 1000, loc = 1, scale = 0.5, shape = -0.2)
+# 
+# results<- estimate_gev_parameters(x, nsloc = NULL)
+# 
+# results
+# names(results)
+# 
+# 
+# # example 3
+# 
+# source("./src/generate_gev_sample.R")
+# 
+# x <- generate_gev_sample(n = 1000, loc = 1, scale = 0.5, shape = 0)
+# 
+# results<- estimate_gev_parameters(x, nsloc = NULL)
+# 
+# results
+# names(results)
