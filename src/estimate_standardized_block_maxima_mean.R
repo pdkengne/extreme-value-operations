94da3c2e20a9af18401cc3537d902d22c3aaef45
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-08-30 22:49:29 +0200

    update of the file estimate_standardized_block_maxima_mean.R

--- src/estimate_standardized_block_maxima_mean.R
+++ src/estimate_standardized_block_maxima_mean.R
@@ -1,12 +1,11 @@
 source("./src/extract_block_maxima.R")
 source("./src/estimate_gev_parameters.R")
 
-# https://www.econometrics-with-r.org/3.4-confidence-intervals-for-the-population-mean.html
 
-estimate_standardized_block_maxima_mean <- function(x, block_size = 1, confidence_interval_level = 0.95){
+estimate_standardized_block_maxima_mean <- function(x, block_size = 1, confidence_level = 0.95){
   # x: vector of observations
   # block_size: size of blocks to consider
-  # confidence_interval_level: desired confidence interval level
+  # confidence_interval_level: desired confidence level
   
   # extract block maxima
   block_maxima <- extract_block_maxima(x, block_size)
@@ -21,22 +20,35 @@ estimate_standardized_block_maxima_mean <- function(x, block_size = 1, confidenc
   standardized_block_maxima <- (block_maxima - gev_model_parameters["loc"])/gev_model_parameters["scale"]
   
   # calculate the confidence interval of the standardized block maxima mean
-  estimated_mean_confidence_interval <- t.test()
-
-
-    estimated_mean_confidence_interval
+  standardized_block_maxima_student_test <- t.test(x = standardized_block_maxima, 
+                                                   alternative = "two.sided",
+                                                   mu = 0, 
+                                                   var.equal = FALSE,
+                                                   conf.level = confidence_level)
+  
+  estimated_mean_confidence_interval <- rep(NA, 3)
+  estimated_mean_confidence_interval[2] <- standardized_block_maxima_student_test$estimate
+  estimated_mean_confidence_interval[c(1,3)] <- standardized_block_maxima_student_test$conf.int
+  names(estimated_mean_confidence_interval) <- c("lower_bound", "estimate", "upper_bound")
+  
+  
+  estimated_mean_confidence_interval
 }
 
-# https://www.econometrics-with-r.org/3.4-confidence-intervals-for-the-population-mean.html
 
-
-
-
-
-
-
-
-
-
-
-
+# # example 1
+# 
+# x <- rnorm(n = 1000)
+# 
+# results <- estimate_standardized_block_maxima_mean(x, block_size = 25, confidence_level = 0.95)
+# 
+# results
+# 
+# 
+# # example 2
+# 
+# x <- rnorm(n = 1000)
+# 
+# results <- estimate_standardized_block_maxima_mean(x, block_size = 40, confidence_level = 0.95)
+# 
+# results
