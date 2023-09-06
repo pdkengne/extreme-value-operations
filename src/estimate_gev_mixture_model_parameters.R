e4da75851a43ba711aa4a2fac00c2e1b07ce9316
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-09-06 04:15:37 +0200

    modification of the file extract_nlargest_sample.R

--- src/estimate_gev_mixture_model_parameters.R
+++ src/estimate_gev_mixture_model_parameters.R
@@ -4,11 +4,13 @@ source("./src/estimate_gev_mixture_model_pessimistic_weights.R")
 source("./src/estimate_several_standardized_block_maxima_mean.R")
 source("./src/estimate_gev_mixture_model_automatic_weights_mw.R")
 source("./src/estimate_gev_mixture_model_automatic_weights_pw.R")
+source("./src/extract_nlargest_sample.R")
 
 estimate_gev_mixture_model_parameters <- function(x, 
                                                   nsloc = NULL, 
                                                   std.err = FALSE, 
                                                   block_sizes = NULL, 
+                                                  nlargest = Inf,
                                                   confidence_level = 0.95,
                                                   trace = TRUE){
   # x: vector of observations
@@ -17,6 +19,7 @@ estimate_gev_mixture_model_parameters <- function(x,
   # trace: boolean value which indicates whether to print information on the progress of optimization
   # std.err: a boolean which indicates whether the standard errors are returned or not
   # confidence_level: desired confidence level
+  # nlargest: number of largest values to focus on. Note that the whole vector x is use unless, nlargest != Inf.
   
   
   
--- src/extract_nlargest_sample.R
+++ src/extract_nlargest_sample.R
@@ -1,13 +1,20 @@
-extract_nlargest_sample <- function(x, n = 10){
+extract_nlargest_sample <- function(x, n = Inf){
   # x: vector of observations
-  # n: number of largest values
+  # n: number of largest values 
+  #    Note that the vector x is returned, unless n != Inf in which case the vector of n largest values of x is returned.
   
-  # get the position of the n-largest values in the vector x
-  position <- sort(tail(order(x), n))
+  if (n == Inf){
+    nlargest_sample <- x
+  }
+  else{
+    # get the position of the n-largest values in the vector x
+    position <- sort(tail(order(x), n))
+    
+    # extract the n-largest values from the vector x
+    nlargest_sample <- x[position]
+    names(nlargest_sample) <- position
+  }
   
-  # extract the n-largest values from the vector x
-  nlargest_sample <- x[position]
-
   nlargest_sample
 }
 
@@ -26,4 +33,11 @@ extract_nlargest_sample <- function(x, n = 10){
 # result <- extract_nlargest_sample(x, n = 10)
 # result
 # x
-
+# 
+# 
+# # example 3
+# 
+# x <- rnorm(n = 20)
+# result <- extract_nlargest_sample(x, n = Inf)
+# result
+# x
