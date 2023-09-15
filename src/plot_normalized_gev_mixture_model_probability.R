070a6e75b2225c8b3f3e57ebcb47b66baa1547a8
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-09-15 22:09:00 +0200

    update file plot_normalized_gev_mixture_model_probability.R

--- src/plot_normalized_gev_mixture_model_probability.R
+++ src/plot_normalized_gev_mixture_model_probability.R
@@ -94,6 +94,7 @@ plot_normalized_gev_mixture_model_probability <- function(x,
   }
   
   abline(a = 0, b = 1, lty = "dotted", lwd = 2)
+  lines(large_theoretical_cdf, large_empirical_cdf, col = 2, lwd = 2)
   
   legend(x = "topleft", 
          legend = c("Line y = x"),
@@ -106,81 +107,8 @@ plot_normalized_gev_mixture_model_probability <- function(x,
 }
 
 
+
 # # example 1
-# 
-# source("./src/generate_gev_sample.R")
-# source("./src/estimate_gev_mixture_model_parameters.R")
-# source("./src/find_threshold_associated_with_given_block_size.R")
-# 
-# n <- 10000
-# nlargest <- 1000
-# 
-# # x <- rnorm(n = n)
-# x <- generate_gev_sample(n = n, loc = 1, scale = 0.5, shape = 0.1)
-# 
-# gev_mixture_model <- estimate_gev_mixture_model_parameters(x,
-#                                                            nsloc = NULL,
-#                                                            std.err = FALSE,
-#                                                            block_sizes = NULL,
-#                                                            minimum_nblocks = 50,
-#                                                            nlargest = nlargest,
-#                                                            confidence_level = 0.95,
-#                                                            log_mv = FALSE,
-#                                                            log_pw = FALSE,
-#                                                            trace = TRUE)
-# 
-# # extract the model parameters (mw)
-# gev_mixture_model_parameters <- gev_mixture_model$normalized_gev_parameters_object
-# gev_mixture_model_parameters_shape <- gev_mixture_model_parameters$shape_star
-# gev_mixture_model_parameters_scale <- gev_mixture_model_parameters$scale_star
-# gev_mixture_model_parameters_loc <- gev_mixture_model_parameters$loc_star
-# 
-# # extract the weight parameters (mw)
-# gev_mixture_model_identic_weights <- gev_mixture_model$identic_weights_mw
-# gev_mixture_model_pessimistic_weights <- gev_mixture_model$pessimistic_weights_mw
-# gev_mixture_model_automatic_weights <- gev_mixture_model$automatic_weights_mw
-# 
-# block_size <- max(gev_mixture_model$block_sizes)
-# 
-# threshold <- find_threshold_associated_with_given_block_size(x = y, block_size = block_size)
-# 
-# y <- gev_mixture_model$data_largest
-# 
-# plot_normalized_gev_mixture_model_probability(x = y, 
-#                                               locations = gev_mixture_model_parameters_loc, 
-#                                               scales = gev_mixture_model_parameters_scale, 
-#                                               shapes = gev_mixture_model_parameters_shape, 
-#                                               weights = gev_mixture_model_automatic_weights,
-#                                               zoom = FALSE,
-#                                               threshold = NULL,
-#                                               xlab = "Theoretical Probability", 
-#                                               ylab = "Empirical Probability", 
-#                                               main = "Probability Plot")
-# 
-# plot_normalized_gev_mixture_model_probability(x = y, 
-#                                               locations = gev_mixture_model_parameters_loc, 
-#                                               scales = gev_mixture_model_parameters_scale, 
-#                                               shapes = gev_mixture_model_parameters_shape, 
-#                                               weights = gev_mixture_model_automatic_weights,
-#                                               zoom = FALSE,
-#                                               threshold = threshold,
-#                                               xlab = "Theoretical Probability", 
-#                                               ylab = "Empirical Probability", 
-#                                               main = "Probability Plot")
-# 
-# plot_normalized_gev_mixture_model_probability(x = y, 
-#                                               locations = gev_mixture_model_parameters_loc, 
-#                                               scales = gev_mixture_model_parameters_scale, 
-#                                               shapes = gev_mixture_model_parameters_shape, 
-#                                               weights = gev_mixture_model_automatic_weights,
-#                                               zoom = TRUE,
-#                                               threshold = threshold,
-#                                               xlab = "Theoretical Probability", 
-#                                               ylab = "Empirical Probability", 
-#                                               main = "Probability Plot")
-# 
-# 
-# # example 2
 # 
 # source("./src/generate_gev_mixture_model_sample.R")
 # 
@@ -197,14 +125,37 @@ plot_normalized_gev_mixture_model_probability <- function(x,
 # 
 # z <- generate_gev_mixture_model_sample(n = n, locations, scales, shapes, weights, iterations = 50)
 # 
+# plot_normalized_gev_mixture_model_probability(x = z,
+#                                               locations = locations,
+#                                               scales = scales,
+#                                               shapes = shapes,
+#                                               weights = weights,
+#                                               zoom = FALSE,
+#                                               threshold = NULL,
+#                                               xlab = "Theoretical Probability",
+#                                               ylab = "Empirical Probability",
+#                                               main = "Probability Plot")
 # 
-# plot_normalized_gev_mixture_model_probability(x = z, 
-#                                               locations = locations, 
-#                                               scales = scales, 
-#                                               shapes = shapes, 
+# threshold <- median(z)
+# 
+# plot_normalized_gev_mixture_model_probability(x = z,
+#                                               locations = locations,
+#                                               scales = scales,
+#                                               shapes = shapes,
 #                                               weights = weights,
 #                                               zoom = FALSE,
 #                                               threshold = threshold,
-#                                               xlab = "Theoretical Probability", 
-#                                               ylab = "Empirical Probability", 
+#                                               xlab = "Theoretical Probability",
+#                                               ylab = "Empirical Probability",
+#                                               main = "Probability Plot")
+# 
+# plot_normalized_gev_mixture_model_probability(x = z,
+#                                               locations = locations,
+#                                               scales = scales,
+#                                               shapes = shapes,
+#                                               weights = weights,
+#                                               zoom = TRUE,
+#                                               threshold = threshold,
+#                                               xlab = "Theoretical Probability",
+#                                               ylab = "Empirical Probability",
 #                                               main = "Probability Plot")
--- src/plot_normalized_gev_probability.R
+++ src/plot_normalized_gev_probability.R
@@ -105,46 +105,46 @@ plot_normalized_gev_probability <- function(x,
 }
 
 
-# example 1
-
-source("./src/generate_gev_sample.R")
-
-loc <- 1
-
-scale <- 0.5
-
-shape <- 0.1
-
-x <- generate_gev_sample(n = 100, loc = loc, scale = scale, shape = shape)
-
-plot_normalized_gev_probability(x,
-                                loc = loc,
-                                scale = scale,
-                                shape = shape,
-                                zoom = FALSE,
-                                threshold = NULL,
-                                xlab = "Theoretical Probability",
-                                ylab = "Empirical Probability",
-                                main = "Probability Plot")
-
-threshold <- median(x)
-
-plot_normalized_gev_probability(x,
-                                loc = loc,
-                                scale = scale,
-                                shape = shape,
-                                zoom = FALSE,
-                                threshold = threshold,
-                                xlab = "Theoretical Probability",
-                                ylab = "Empirical Probability",
-                                main = "Probability Plot")
-
-plot_normalized_gev_probability(x,
-                                loc = loc,
-                                scale = scale,
-                                shape = shape,
-                                zoom = TRUE,
-                                threshold = threshold,
-                                xlab = "Theoretical Probability",
-                                ylab = "Empirical Probability",
-                                main = "Probability Plot")
+# # example 1
+# 
+# source("./src/generate_gev_sample.R")
+# 
+# loc <- 1
+# 
+# scale <- 0.5
+# 
+# shape <- 0.1
+# 
+# x <- generate_gev_sample(n = 100, loc = loc, scale = scale, shape = shape)
+# 
+# plot_normalized_gev_probability(x,
+#                                 loc = loc,
+#                                 scale = scale,
+#                                 shape = shape,
+#                                 zoom = FALSE,
+#                                 threshold = NULL,
+#                                 xlab = "Theoretical Probability",
+#                                 ylab = "Empirical Probability",
+#                                 main = "Probability Plot")
+# 
+# threshold <- median(x)
+# 
+# plot_normalized_gev_probability(x,
+#                                 loc = loc,
+#                                 scale = scale,
+#                                 shape = shape,
+#                                 zoom = FALSE,
+#                                 threshold = threshold,
+#                                 xlab = "Theoretical Probability",
+#                                 ylab = "Empirical Probability",
+#                                 main = "Probability Plot")
+# 
+# plot_normalized_gev_probability(x,
+#                                 loc = loc,
+#                                 scale = scale,
+#                                 shape = shape,
+#                                 zoom = TRUE,
+#                                 threshold = threshold,
+#                                 xlab = "Theoretical Probability",
+#                                 ylab = "Empirical Probability",
+#                                 main = "Probability Plot")
