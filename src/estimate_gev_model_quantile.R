6d430bf612279a37cb43a1899ae8b073422a92ef
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-09-25 19:50:50 +0200

    creation of the file estimate_gev_model_quantile.R

--- src/estimate_gev_model_quantile.R
+++ src/estimate_gev_model_quantile.R
@@ -1,4 +1,55 @@
-library(extRemes)
+# library(extRemes)
+
+source("./src/estimate_gev_model_parameters.R")
+
+
+estimate_gev_model_quantile <- function(x, 
+                                        data, 
+                                        alpha = NULL,
+                                        do.ci = TRUE,
+                                        confidence_level = 0.95,
+                                        qcov = NULL, 
+                                        threshold = NULL, 
+                                        threshold.fun = ~1, 
+                                        location.fun = ~1,
+                                        scale.fun = ~1, 
+                                        shape.fun = ~1, 
+                                        use.phi = FALSE,
+                                        type = c("GEV", "GP", "PP", "Gumbel", "Exponential")[1],
+                                        method = c("MLE", "GMLE", "Bayesian", "Lmoments")[1]){
+  # x: vector of observations (assumed to be block maxima)
+  # data: dataframe of covariates for linear modeling of the location parameter
+  # alpha: order of the quantile to estimate
+  # do.ci: boolean which indicates whether to return confidence interval or not
+  # confidence_level: the desired confidence level for the estimated quantile
+  # qcov: numeric matrix with rows the same length as q and columns equal to the number of parameters.
+  #       This gives any covariate values for a nonstationary model
+  # threshold.fun, location.fun, scale.fun, shape.fun: formula describing a model for each parameter using columns from data. data
+  #                                                    must be supplied if any of these arguments are anything other than ~ 1.
+  # use.phi: boolean which indicates whether to use the log of the scale parameter or not
+  # type: type of model to use
+  # method: estimation mmethod to use
+  
+  model <- estimate_gev_model_parameters(x = x, 
+                                         data = data, 
+                                         threshold = threshold, 
+                                         threshold.fun = threshold.fun, 
+                                         location.fun = location.fun,
+                                         scale.fun = scale.fun, 
+                                         shape.fun = shape.fun, 
+                                         use.phi = use.phi,
+                                         type = type,
+                                         method = method)
+  
+  quantile <- return.level(x = model, 
+                           return.period = 1/alpha, 
+                           alpha = 1 - confidence_level, 
+                           method = c("normal"), 
+                           do.ci = do.ci,
+                           qcov = qcov)
+  
+  quantile
+}
 
 
 
@@ -43,11 +94,11 @@ return.level(x,
              alpha = 0.05, 
              method = c("normal"), 
              do.ci = FALSE, 
-             verbose = FALSE,
+             verbose = TRUE,
              qcov = NULL, 
              qcov.base = NULL)
 
-rl <- return.level(fit, return.period = 1000000000, do.ci = TRUE)
+rl <- return.level(fit, return.period = 100, do.ci = TRUE)
 
 rl
 
