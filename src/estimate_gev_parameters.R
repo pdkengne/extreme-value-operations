1280a09e3278172500c61214b1a76a48a344b41b
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-10-04 15:37:36 +0200

    handle covariates in the function estimate_gev_parameters

--- src/estimate_gev_parameters.R
+++ src/estimate_gev_parameters.R
@@ -1,32 +1,41 @@
-# library(evd)
-
-source("./src/estimate_gev_model_parameters.R")
+# library(extRemes)
 
 options(digits = 10)
 
-estimate_gev_parameters <- function(x, ..., nsloc = NULL, prob = NULL, std.err = FALSE){
+estimate_gev_parameters <- function(x, 
+                                    data = NULL, 
+                                    threshold = NULL, 
+                                    threshold.fun = ~1, 
+                                    location.fun = ~1,
+                                    scale.fun = ~1, 
+                                    shape.fun = ~1, 
+                                    use.phi = FALSE,
+                                    type = c("GEV", "GP", "PP", "Gumbel", "Exponential")[1],
+                                    method = c("MLE", "GMLE", "Bayesian", "Lmoments")[1]){
   # x: vector of observations (assumed to be block maxima)
-  # nsloc: dataframe of covariates for linear modeling of the location parameter
-  # prob: order of quantile associated with the parametrization (quantile, scale, shape)
-  # std.err: a boolean which indicates whether the standard errors are returned or not
+  # data: dataframe of covariates for linear modeling of the location parameter
+  # threshold.fun, location.fun, scale.fun, shape.fun: formula describing a model for each parameter using columns from data. data
+  #                                                    must be supplied if any of these arguments are anything other than ~ 1.
+  # use.phi: boolean which indicates whether to use the log of the scale parameter or not
+  # type: type of model to use
+  # method: estimation method to use
   
-  initial_estimate <- estimate_gev_model_parameters(x = x)
+  # check whether data is null
+  if (is.null(data)){
+    data <- data.frame("x" = x)
+  }
   
-  start_parameters <- initial_estimate$results$par
-  
-  start <- list(loc = start_parameters["location"],
-                scale = start_parameters["scale"],
-                shape = start_parameters["shape"])
-  
-  gev_model <- evd::fgev(x, 
-                         start = start, 
-                         ..., 
-                         nsloc = nsloc, 
-                         prob = prob, 
-                         std.err = std.err,
-                         corr = FALSE, 
-                         method = "BFGS", 
-                         warn.inf = TRUE)
+  # estimation of the gev model
+  gev_model <- extRemes::fevd(x = x, 
+                              data = data, 
+                              threshold = threshold, 
+                              threshold.fun = threshold.fun, 
+                              location.fun = location.fun,
+                              scale.fun = scale.fun, 
+                              shape.fun = shape.fun, 
+                              use.phi = use.phi,
+                              type = type,
+                              method = method)
   
   gev_model
 }
@@ -39,7 +48,7 @@ estimate_gev_parameters <- function(x, ..., nsloc = NULL, prob = NULL, std.err =
 # 
 # x <- generate_gev_sample(n = 1000, loc = 1, scale = 0.5, shape = +0.2)
 # 
-# results<- estimate_gev_parameters(x, nsloc = NULL, std.err = FALSE)
+# results <- estimate_gev_parameters(x)
 # 
 # results
 # names(results)
@@ -51,7 +60,7 @@ estimate_gev_parameters <- function(x, ..., nsloc = NULL, prob = NULL, std.err =
 # 
 # x <- generate_gev_sample(n = 1000, loc = 1, scale = 0.5, shape = -0.2)
 # 
-# results<- estimate_gev_parameters(x, nsloc = NULL, std.err = TRUE)
+# results <- estimate_gev_parameters(x, method = c("MLE", "GMLE", "Bayesian", "Lmoments")[4])
 # 
 # results
 # names(results)
@@ -63,7 +72,7 @@ estimate_gev_parameters <- function(x, ..., nsloc = NULL, prob = NULL, std.err =
 # 
 # x <- generate_gev_sample(n = 1000, loc = 1, scale = 0.5, shape = 0)
 # 
-# results<- estimate_gev_parameters(x, nsloc = NULL)
+# results <- estimate_gev_parameters(x, method = c("MLE", "GMLE", "Bayesian", "Lmoments")[2])
 # 
 # results
 # names(results)
@@ -73,10 +82,22 @@ estimate_gev_parameters <- function(x, ..., nsloc = NULL, prob = NULL, std.err =
 # 
 # source("./src/generate_gev_sample.R")
 # 
-# x <- generate_gev_sample(n = 1000, loc = 1, scale = 0.5, shape = 0)
+# x <- generate_gev_sample(n = 100, loc = 0.13, scale = 1.1, shape = 0.2)
 # 
-# results<- estimate_gev_parameters(x, nsloc = NULL, prob = 0.02, scale = 0.5, shape = 0, std.err = TRUE)
+# trend <- (-49:50)/100
+# rnd <- runif(100, min = -0.5, max = 0.5)
+# data <- data.frame(trend = trend, random = rnd)
+# 
+# results <- estimate_gev_parameters(x = x, 
+#                                   data = data, 
+#                                   location.fun = ~ .,
+#                                   scale.fun = ~ ., 
+#                                   shape.fun = ~ ., 
+#                                   use.phi = TRUE,
+#                                   method = c("MLE", "GMLE", "Bayesian", "Lmoments")[1])
 # 
 # results
 # names(results)
-# confint(object = results, level = 0.95)
+# summary <- summary(results, silent = TRUE)
+# summary
+# names(summary)
