82c7c1439bdf0e74cd715baf3dc1b60ad5042afd
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-10-09 01:36:20 +0200

    update file estimate_single_ns_gev_model.R

--- src/estimate_single_ns_gev_model.R
+++ src/estimate_single_ns_gev_model.R
@@ -1,4 +1,3 @@
-source("./src/extract_block_maxima.R")
 source("./src/extract_block_maxima_with_indexes.R")
 source("./src/estimate_gev_parameters.R")
 source("./src/estimate_ns_gev_parameters.R")
@@ -36,32 +35,42 @@ estimate_single_ns_gev_model <- function(x,
   # get the index of block maxima
   block_maxima_indexes <- block_maxima_with_indexes$block_maxima_indexes
   
-  # check whether data is null
+  # estimate the (non stationary) gev model
   if (is.null(data)){
     data <- data.frame("x" = x)
+    
+    # extract covariates associated to block maxima
+    block_maxima_covariates <- data.frame(data[block_maxima_indexes, ])
+    rownames(block_maxima_covariates) <- block_maxima_indexes
+    names(block_maxima_covariates) <- names(data)
+    
+    gev_model <- estimate_ns_gev_parameters(x = block_maxima,
+                                            type = type,
+                                            method = method)
   }
-  
-  # extract covariates associated to block maxima
-  block_maxima_covariates <- data.frame(data[block_maxima_indexes, ])
-  rownames(block_maxima_covariates) <- block_maxima_indexes
-  names(block_maxima_covariates) <- names(data)
+  else{
+    # extract covariates associated to block maxima
+    block_maxima_covariates <- data.frame(data[block_maxima_indexes, ])
+    rownames(block_maxima_covariates) <- block_maxima_indexes
+    names(block_maxima_covariates) <- names(data)
+    
+    gev_model <- estimate_ns_gev_parameters(x = block_maxima,
+                                            data = block_maxima_covariates, 
+                                            location.fun = location.fun,
+                                            scale.fun = scale.fun, 
+                                            shape.fun = shape.fun, 
+                                            use.phi = use.phi,
+                                            type = type,
+                                            method = method)
+  }
   
   # calculate the extremal index
   threshold <- find_threshold_associated_with_given_block_size(x, block_size)
   extremal_index <- estimate_extremal_index(x = x, threshold = threshold)
   
-  # estimate gev model
-  gev_model <- estimate_ns_gev_parameters(x = block_maxima,
-                                          data = block_maxima_covariates, 
-                                          location.fun = location.fun,
-                                          scale.fun = scale.fun, 
-                                          shape.fun = shape.fun, 
-                                          use.phi = use.phi,
-                                          type = type,
-                                          method = method)
-  
   # update the output object
   output[["data"]] <- x
+  output[["covariates"]] <- data
   output[["block_maxima_covariates"]] <- block_maxima_covariates
   output[["block_size"]] <- block_size
   output[["block_maxima"]] <- block_maxima
@@ -82,15 +91,11 @@ estimate_single_ns_gev_model <- function(x,
 # 
 # x <- generate_gev_sample(n = n, loc = 1, scale = 0.5, shape = +0.2)
 # 
-# trend <- (-49:50)/n
-# rnd <- runif(n = n, min = -0.5, max = 0.5)
-# data <- data.frame(trend = trend, random = rnd)
-# 
 # block_size <- 25
 # 
 # results<- estimate_single_ns_gev_model(x = x,
 #                                        block_size = block_size,
-#                                        data = data,
+#                                        data = NULL,
 #                                        location.fun = ~ .,
 #                                        scale.fun = ~1,
 #                                        shape.fun = ~1,
@@ -116,6 +121,9 @@ estimate_single_ns_gev_model <- function(x,
 # 
 # # get block maxima covariates
 # results$block_maxima_covariates
+# 
+# # get the full covariates
+# results$covariates
 # 
 # # get gev model
 # model<- results$gev_model
@@ -167,6 +175,9 @@ estimate_single_ns_gev_model <- function(x,
 # # get block maxima covariates
 # results$block_maxima_covariates
 # 
+# # get the full covariates
+# results$covariates
+# 
 # # get gev model
 # model<- results$gev_model
 # 
@@ -217,55 +228,8 @@ estimate_single_ns_gev_model <- function(x,
 # # get block maxima covariates
 # results$block_maxima_covariates
 # 
-# # get gev model
-# model<- results$gev_model
-# 
-# model
-# 
-# names(model)
-# 
-# 
-# # example 4
-# 
-# source("./src/generate_gev_sample.R")
-# 
-# n <- 1000
-# 
-# x <- generate_gev_sample(n = n, loc = 0.13, scale = 1.1, shape = 0)
-# 
-# trend <- (-49:50)/n
-# rnd <- runif(n = n, min = -0.5, max = 0.5)
-# data <- data.frame(trend = trend, random = rnd)
-# 
-# block_size <- 40
-# 
-# results <- estimate_single_ns_gev_model(x = x,
-#                                         block_size = block_size,
-#                                         data = data,
-#                                         location.fun = ~ .,
-#                                         scale.fun = ~ .,
-#                                         shape.fun = ~ 1,
-#                                         use.phi = TRUE,
-#                                         type = c("GEV", "Gumbel")[1],
-#                                         method = c("MLE", "GMLE")[1])
-# 
-# #results
-# names(results)
-# 
-# # get the block size
-# results$block_size
-# 
-# # get the extremal index
-# results$extremal_index
-# 
-# # get block maxima
-# results$block_maxima
-# 
-# # get block maxima indexes
-# results$block_maxima_indexes
-# 
-# # get block maxima covariates
-# results$block_maxima_covariates
+# # get the full covariates
+# results$covariates
 # 
 # # get gev model
 # model<- results$gev_model
