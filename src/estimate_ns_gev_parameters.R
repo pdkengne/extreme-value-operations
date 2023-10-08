ef45724c96fe939fa7ea3ac495ba2b904667053c
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-10-09 00:54:19 +0200

    update file estimate_ns_gev_parameters.R

--- src/estimate_ns_gev_parameters.R
+++ src/estimate_ns_gev_parameters.R
@@ -18,20 +18,23 @@ estimate_ns_gev_parameters <- function(x,
   # type: type of model to use
   # method: estimation method to use
   
-  # check whether data is null
+  # estimate the (non stationary) gev model
   if (is.null(data)){
-    data <- data.frame("x" = x)
+    gev_model <- extRemes::fevd(x = x, 
+                                type = type,
+                                method = method)
+    
   }
-  
-  # estimation of the gev model
-  gev_model <- extRemes::fevd(x = x, 
-                              data = data, 
-                              location.fun = location.fun,
-                              scale.fun = scale.fun, 
-                              shape.fun = shape.fun, 
-                              use.phi = use.phi,
-                              type = type,
-                              method = method)
+  else{
+    gev_model <- extRemes::fevd(x = x, 
+                                data = data, 
+                                location.fun = location.fun,
+                                scale.fun = scale.fun, 
+                                shape.fun = shape.fun, 
+                                use.phi = use.phi,
+                                type = type,
+                                method = method)
+  }
   
   gev_model
 }
@@ -42,13 +45,16 @@ estimate_ns_gev_parameters <- function(x,
 # 
 # source("./src/generate_gev_sample.R")
 # 
-# x <- generate_gev_sample(n = 1000, loc = 1, scale = 0.5, shape = +0.2)
+# n <- 1000
+# 
+# x <- generate_gev_sample(n = n, loc = 1, scale = 0.5, shape = +0.2)
+# 
 # 
 # results <- estimate_ns_gev_parameters(x,
 #                                       data = NULL,
 #                                       location.fun = ~1,
-#                                       scale.fun = ~1, 
-#                                       shape.fun = ~1, 
+#                                       scale.fun = ~1,
+#                                       shape.fun = ~1,
 #                                       use.phi = TRUE,
 #                                       type = c("GEV", "Gumbel")[1],
 #                                       method = c("MLE", "GMLE")[1])
@@ -61,16 +67,22 @@ estimate_ns_gev_parameters <- function(x,
 # 
 # source("./src/generate_gev_sample.R")
 # 
-# x <- generate_gev_sample(n = 1000, loc = 1, scale = 0.5, shape = -0.2)
+# n <- 1000
+# 
+# x <- generate_gev_sample(n = n, loc = 1, scale = 0.5, shape = -0.2)
+# 
+# trend <- (-49:50)/n
+# rnd <- runif(n = n, min = -0.5, max = 0.5)
+# data <- data.frame(trend = trend, random = rnd)
 # 
 # results <- estimate_ns_gev_parameters(x,
-#                                       data = NULL,
-#                                       location.fun = ~1,
-#                                       scale.fun = ~1, 
-#                                       shape.fun = ~1, 
+#                                       data = data,
+#                                       location.fun = ~ .,
+#                                       scale.fun = ~1,
+#                                       shape.fun = ~1,
 #                                       use.phi = TRUE,
 #                                       type = c("GEV", "Gumbel")[1],
-#                                       method = c("MLE", "GMLE")[2])
+#                                       method = c("MLE", "GMLE")[1])
 # 
 # results
 # names(results)
@@ -80,45 +92,22 @@ estimate_ns_gev_parameters <- function(x,
 # 
 # source("./src/generate_gev_sample.R")
 # 
-# x <- generate_gev_sample(n = 100, loc = 0.13, scale = 1.1, shape = 0)
+# n <- 1000
 # 
-# trend <- (-49:50)/100
-# rnd <- runif(100, min = -0.5, max = 0.5)
+# x <- generate_gev_sample(n = n, loc = 1, scale = 0.5, shape = 0)
+# 
+# trend <- (-49:50)/n
+# rnd <- runif(n = n, min = -0.5, max = 0.5)
 # data <- data.frame(trend = trend, random = rnd)
 # 
-# results <- estimate_ns_gev_parameters(x = x,
+# results <- estimate_ns_gev_parameters(x,
 #                                       data = data,
 #                                       location.fun = ~ .,
 #                                       scale.fun = ~ .,
-#                                       shape.fun = ~ .,
-#                                       use.phi = FALSE,
-#                                       method = c("MLE", "GMLE")[1])
-# 
-# results
-# names(results)
-# summary <- summary(results, silent = TRUE)
-# summary
-# names(summary)
-# 
-# 
-# 
-# # example 4
-# 
-# source("./src/generate_gev_sample.R")
-# 
-# x <- generate_gev_sample(n = 100, loc = 0.13, scale = 1.1, shape = 0)
-# 
-# trend <- (-49:50)/100
-# rnd <- runif(100, min = -0.5, max = 0.5)
-# data <- data.frame(trend = trend, random = rnd)
-# 
-# results <- estimate_ns_gev_parameters(x = x,
-#                                       data = data,
-#                                       location.fun = ~ .,
-#                                       scale.fun = ~ .,
-#                                       shape.fun = ~ .,
+#                                       shape.fun = ~1,
 #                                       use.phi = TRUE,
-#                                       method = c("MLE", "GMLE")[1])
+#                                       type = c("GEV", "Gumbel")[1],
+#                                       method = c("MLE", "GMLE")[2])
 # 
 # results
 # names(results)
