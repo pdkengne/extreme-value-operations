0ab3ecfdcd8383f02dc6407ce24e2b2f4e31b66a
Dkengne Sielenou Pascal Alain <sielenou_alain@yahoo.fr>
2023-10-19 16:34:56 +0200

    change library evd by extremes in the function calculate_gev_pdf

--- src/calculate_gev_pdf.R
+++ src/calculate_gev_pdf.R
@@ -1,10 +1,14 @@
-# library(evd)
+# library(extRemes)
 
 calculate_gev_pdf <- function(x, loc = 0, scale = 1, shape = 0){
   # x: vector of observations
   # loc, scale, shape: location, scale and shape parameters of the considered gev distribution
   
-  gev_pdf <- evd::dgev(x, loc, scale, shape)
+  gev_pdf <- extRemes::devd(x = x, 
+                            loc = loc, 
+                            scale = scale, 
+                            shape = shape, 
+                            type = "GEV")
   
   gev_pdf
 }
@@ -13,7 +17,7 @@ calculate_gev_pdf <- function(x, loc = 0, scale = 1, shape = 0){
 
 # # example 1
 # 
-# result <- calculate_gev_pdf(x = 2:4, loc = 1, scale = 0.5, shape = 0.8)
+# result <- calculate_gev_pdf(x = 2:4, loc = 1, scale = 0.5, shape = +0.2)
 # 
 # result
 # 
