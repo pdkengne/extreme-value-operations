1a1adb108f2548adb49b716a1c91011b7624b755
pdkengne <sielenou_alain@yahoo.fr>
2023-10-30 18:56:18 +0100

    creation of the file get_one_hot_encoder.R

--- src/get_one_hot_encoder.R
+++ src/get_one_hot_encoder.R
@@ -1,25 +1,35 @@
 library(caret)
 
-customers <- data.frame(
-  id=c(10,20,30,40,50),
-  gender=c('male','female','female','male','female'),
-  mood=c('happy','sad','happy','sad','happy'),
-  outcome=c(1,1,0,0,0))
+get_one_hot_encoder <- function(data){
+  # data: A data frame with the predictors of interest
+  
+  # Convert all character columns to factor
+  data <- as.data.frame(unclass(data), stringsAsFactors = TRUE)
+  
+  output <- caret::dummyVars(formula = " ~ .", sep = NULL, data = data)
+  
+  output
+}
 
 
-customers
-
-# dummify the data
-dummy <- caret::dummyVars(formula = " ~ .", sep = NULL, data = customers)
-
-dummy
-
-transformed <- data.frame(predict(object = dummy, newdata = customers))
-
-transformed
-
-
-
-
-
-
+# # example 1
+# 
+# data <- data.frame(id=c(10,20,30,40,50),
+#                    gender=c('male','female','female','male','female'),
+#                    mood=c('happy','sad','happy','sad','happy'),
+#                    outcome=c(1,1,0,0,0))
+# 
+# str(data)
+# 
+# result <- get_one_hot_encoder(data = data)
+# 
+# result
+# 
+# transformed_data <- data.frame(predict(object = result, newdata = data))
+# 
+# transformed_data
+# 
+# 
+# transformed_data <- data.frame(predict(object = result, newdata = data[1, ], contrasts = FALSE))
+# 
+# transformed_data
