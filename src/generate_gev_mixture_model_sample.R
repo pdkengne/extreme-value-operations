b14be5c082170229c3ac007e6b19381102c70d3a
pdkengne <sielenou_alain@yahoo.fr>
2023-12-10 18:55:42 +0100

    update examples in the function generate_gev_mixture_model_sample

--- src/generate_gev_mixture_model_sample.R
+++ src/generate_gev_mixture_model_sample.R
@@ -66,73 +66,11 @@ generate_gev_mixture_model_sample <- function(n = 1,
 # 
 # weights <- c(0.5, 0.5)
 # 
-# shapes <- c(0.1, 0.1)
+# shapes <- c(0.01, 0.1)
 # scales <- c(1, 1)
-# locations <- c(-2, +2)
+# locations <- c(-2, 4)
 # 
-# n <- 1000
-# 
-# results_1 <- generate_gev_mixture_model_sample(n = n,
-#                                              locations,
-#                                              scales,
-#                                              shapes,
-#                                              weights,
-#                                              kind = c("geometric", "arithmetic")[2])
-# 
-# #results_1
-# 
-# pdf_1 <- calculate_gev_mixture_model_pdf(x = sort(results_1),
-#                                        locations,
-#                                        scales,
-#                                        shapes,
-#                                        weights,
-#                                        kind = c("geometric", "arithmetic")[2])
-# 
-# #pdf_1
-# 
-# results_2 <- generate_gev_mixture_model_sample(n = n,
-#                                              locations,
-#                                              scales,
-#                                              shapes,
-#                                              weights,
-#                                              kind = c("geometric", "arithmetic")[1])
-# 
-# #results_2
-# 
-# pdf_2 <- calculate_gev_mixture_model_pdf(x = sort(results_2),
-#                                        locations,
-#                                        scales,
-#                                        shapes,
-#                                        weights,
-#                                        kind = c("geometric", "arithmetic")[1])
-# 
-# #pdf_2
-# 
-# plot(sort(results_1), pdf_1, type = "l", ylim = range(c(pdf_1, pdf_2)), xlim = range(c(results_1, results_2)))
-# lines(sort(results_2), pdf_2, type = "l", col = 4)
-# legend("topright", legend = c("arithmetic", "geometric"), col = c(1, 4), lty = c(1, 1))
-# 
-# results <- generate_gev_mixture_model_sample(n = n,
-#                                              locations,
-#                                              scales,
-#                                              shapes,
-#                                              weights,
-#                                              kind = "merge")
-# 
-# 
-# 
-# # example 2
-# 
-# source("./src/calculate_gev_mixture_model_pdf.R")
-# 
-# weights <- c(0.5, 0.5, 0.5)
-# 
-# shapes <- c(0.1, 0.1, 0.1)
-# scales <- c(1, 1, 1)
-# 
-# locations <- c(-2, +2, +6)
-# 
-# n <- 1500
+# n <- 10000
 # 
 # results_1 <- generate_gev_mixture_model_sample(n = n,
 #                                                locations,
@@ -152,6 +90,24 @@ generate_gev_mixture_model_sample <- function(n = 1,
 # 
 # #pdf_1
 # 
+# density_1 <- density(results_1)
+# 
+# support_density_1 <- density_1$x
+# value_density_1 <- density_1$y
+# 
+# hist(x = results_1, 
+#      freq = FALSE, 
+#      ylim = range(c(value_density_1, pdf_1)),
+#      ylab = "density",
+#      xlab = "support",
+#      main = "mixture model density plot")
+# 
+# lines(x = support_density_1, y = value_density_1, col = 1, lwd = 2)
+# lines(x = sort(results_1), y = pdf_1, col = 4, lwd = 2)
+# 
+# legend("topright", legend = c("empirical", "theoretical"), col = c(1, 4), lty = c(1, 1))
+# 
+# 
 # results_2 <- generate_gev_mixture_model_sample(n = n,
 #                                                locations,
 #                                                scales,
@@ -170,6 +126,145 @@ generate_gev_mixture_model_sample <- function(n = 1,
 # 
 # #pdf_2
 # 
-# plot(sort(results_1), pdf_1, type = "l", ylim = range(c(pdf_1, pdf_2)), xlim = range(c(results_1, results_2)))
-# lines(sort(results_2), pdf_2, type = "l", col = 4)
-# legend("topright", legend = c("arithmetic", "geometric"), col = c(1, 4), lty = c(1, 1))
+# density_2 <- density(results_2)
+# 
+# support_density_2 <- density_2$x
+# value_density_2 <- density_2$y
+# 
+# hist(x = results_2, 
+#      freq = FALSE, 
+#      ylim = range(c(value_density_2, pdf_2)),
+#      ylab = "density",
+#      xlab = "support",
+#      main = "mixture model density plot")
+# 
+# lines(x = support_density_2, y = value_density_2, col = 1, lwd = 2)
+# lines(x = sort(results_2), y = pdf_2, col = 4, lwd = 2)
+# 
+# legend("topright", legend = c("empirical", "theoretical"), col = c(1, 4), lty = c(1, 1))
+# 
+# 
+# plot(x = sort(results_1), 
+#      y = pdf_1, 
+#      type = "l", 
+#      col = 7,
+#      lwd = 2,
+#      ylim = range(c(pdf_1, pdf_2)), 
+#      xlim = range(c(results_1, results_2)),
+#      xlab = "support",
+#      ylab = "density",
+#      main = "mixture model density plot")
+# 
+# lines(x = sort(results_2), y  = pdf_2, type = "l", col = 6, lwd = 2)
+# 
+# legend("topright", legend = c("arithmetic", "geometric"), col = c(7, 6), lty = c(1, 1))
+# 
+# 
+# results <- generate_gev_mixture_model_sample(n = n,
+#                                              locations,
+#                                              scales,
+#                                              shapes,
+#                                              weights,
+#                                              kind = "merge")
+# 
+# 
+# 
+# # example 2
+# 
+# source("./src/calculate_gev_mixture_model_pdf.R")
+# 
+# weights <- c(0.5, 0.5, 0.5)
+# 
+# shapes <- c(0.001, 0.01, 0.1)
+# scales <- c(1, 1, 1)
+# 
+# locations <- c(-2, +4, +10)
+# 
+# n <- 10000
+# 
+# results_1 <- generate_gev_mixture_model_sample(n = n,
+#                                                locations,
+#                                                scales,
+#                                                shapes,
+#                                                weights,
+#                                                kind = c("geometric", "arithmetic")[2])
+# 
+# #results_1
+# 
+# pdf_1 <- calculate_gev_mixture_model_pdf(x = sort(results_1),
+#                                          locations,
+#                                          scales,
+#                                          shapes,
+#                                          weights,
+#                                          kind = c("geometric", "arithmetic")[2])
+# 
+# #pdf_1
+# 
+# density_1 <- density(results_1)
+# 
+# support_density_1 <- density_1$x
+# value_density_1 <- density_1$y
+# 
+# hist(x = results_1, 
+#      freq = FALSE, 
+#      ylim = range(c(value_density_1, pdf_1)),
+#      ylab = "density",
+#      xlab = "support",
+#      main = "mixture model density plot")
+# 
+# lines(x = support_density_1, y = value_density_1, col = 1, lwd = 2)
+# lines(x = sort(results_1), y = pdf_1, col = 4, lwd = 2)
+# 
+# legend("topright", legend = c("empirical", "theoretical"), col = c(1, 4), lty = c(1, 1))
+# 
+# 
+# results_2 <- generate_gev_mixture_model_sample(n = n,
+#                                                locations,
+#                                                scales,
+#                                                shapes,
+#                                                weights,
+#                                                kind = c("geometric", "arithmetic")[1])
+# 
+# #results_2
+# 
+# pdf_2 <- calculate_gev_mixture_model_pdf(x = sort(results_2),
+#                                          locations,
+#                                          scales,
+#                                          shapes,
+#                                          weights,
+#                                          kind = c("geometric", "arithmetic")[1])
+# 
+# #pdf_2
+# 
+# density_2 <- density(results_2)
+# 
+# support_density_2 <- density_2$x
+# value_density_2 <- density_2$y
+# 
+# hist(x = results_2, 
+#      freq = FALSE, 
+#      ylim = range(c(value_density_2, pdf_2)),
+#      ylab = "density",
+#      xlab = "support",
+#      main = "mixture model density plot")
+# 
+# lines(x = support_density_2, y = value_density_2, col = 1, lwd = 2)
+# lines(x = sort(results_2), y = pdf_2, col = 4, lwd = 2)
+# 
+# legend("topright", legend = c("empirical", "theoretical"), col = c(1, 4), lty = c(1, 1))
+# 
+# 
+# plot(x = sort(results_1), 
+#      y = pdf_1, 
+#      type = "l", 
+#      col = 7,
+#      lwd = 2,
+#      ylim = range(c(pdf_1, pdf_2)), 
+#      xlim = range(c(results_1, results_2)),
+#      xlab = "support",
+#      ylab = "density",
+#      main = "mixture model density plot")
+# 
+# lines(x = sort(results_2), y  = pdf_2, type = "l", col = 6, lwd = 2)
+# 
+# legend("topright", legend = c("arithmetic", "geometric"), col = c(7, 6), lty = c(1, 1))
