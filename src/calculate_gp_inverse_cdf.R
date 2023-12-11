480ec5f7eda71485d3fbda9c7b34cb201302b807
pdkengne <sielenou_alain@yahoo.fr>
2023-12-11 16:18:50 +0100

    update the function calculate_gp_inverse_cdf

--- src/calculate_gp_inverse_cdf.R
+++ src/calculate_gp_inverse_cdf.R
@@ -1,12 +1,16 @@
-# library(evd)
+# library(extRemes)
 
-options(digits = 15)
+options(digits = 10)
 
-calculate_gp_inverse_cdf <- function(p, scale = 1, shape = 0, loc = 0){
+calculate_gp_inverse_cdf <- function(p, scale = 1, shape = 0, threshold = 0){
   # p: vector of probabilities
-  # scale, shape, loc: scale, shape and loc parameters of the considered gp distribution
+  # scale, shape, threshold: scale, shape and threshold parameters of the considered gp distribution
   
-  gp_inverse_cdf <- evd::qgpd(p, loc, scale, shape)
+  gp_inverse_cdf <- extRemes::qevd(p = p, 
+                                   threshold = threshold, 
+                                   scale = scale, 
+                                   shape = shape, 
+                                   type = "GP")
   
   gp_inverse_cdf
 }
@@ -17,7 +21,7 @@ calculate_gp_inverse_cdf <- function(p, scale = 1, shape = 0, loc = 0){
 # 
 # probabilities <- seq(from = 10^(-8), to = 1 -10^(-8), length.out = 20)
 # 
-# result <- calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = 0.8, loc = 1)
+# result <- calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = 0.8, threshold = 1)
 # 
 # result
 # 
@@ -26,7 +30,7 @@ calculate_gp_inverse_cdf <- function(p, scale = 1, shape = 0, loc = 0){
 # 
 # probabilities <- seq(from = 10^(-8), to = 1 -10^(-8), length.out = 20)
 # 
-# result <- calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = 0, loc = 1)
+# result <- calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = 0, threshold = 1)
 # 
 # result
 # 
@@ -35,7 +39,7 @@ calculate_gp_inverse_cdf <- function(p, scale = 1, shape = 0, loc = 0){
 # 
 # probabilities <- seq(from = 10^(-8), to = 1 -10^(-8), length.out = 20)
 # 
-# result <- calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = -0.2, loc = 1)
+# result <- calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = -0.2, threshold = 1)
 # 
 # result
 # 
@@ -47,8 +51,8 @@ calculate_gp_inverse_cdf <- function(p, scale = 1, shape = 0, loc = 0){
 # probabilities <- seq(from = 1, to = 9, by = 1)/10
 # probabilities
 # 
-# result <- calculate_gp_cdf(q = calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = 0.8, loc = 1),
-#                             scale = 0.5, shape = 0.8, loc = 1)
+# result <- calculate_gp_cdf(q = calculate_gp_inverse_cdf(p = probabilities, scale = 0.5, shape = 0.8, threshold = 1),
+#                             scale = 0.5, shape = 0.8, threshold = 1)
 # 
 # result
 # 
