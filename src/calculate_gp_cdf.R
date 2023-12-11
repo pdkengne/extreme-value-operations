83d28bf0a91eac27a258aa1caec2aded6114bb60
pdkengne <sielenou_alain@yahoo.fr>
2023-12-11 14:52:32 +0100

    update the function calculate_gp_cdf

--- src/calculate_gp_cdf.R
+++ src/calculate_gp_cdf.R
@@ -1,31 +1,35 @@
-# library(evd)
+# library(extRemes)
 
-calculate_gp_cdf <- function(q, scale = 1, shape = 0, loc = 0){
+calculate_gp_cdf <- function(q, scale = 1, shape = 0, threshold = 0){
   # x: vector of observations
-  # scale, shape, loc: scale, shape and loc parameters of the considered gp distribution
+  # scale, shape, threshold: scale, shape and threshold parameters of the considered gp distribution
   
-  gp_cdf <- evd::pgpd(q, loc, scale, shape)
+  gp_cdf <- extRemes::pevd(q = q, 
+                           threshold = threshold, 
+                           scale = scale, 
+                           shape = shape, 
+                           type = "GP")
   
   gp_cdf
 }
 
 
-# # example 1
-# 
-# result <- calculate_gp_cdf(q = 2:4, scale = 0.5, shape = 0.8, loc = 1)
-# 
-# result
-# 
-# 
-# # example 2
-# 
-# result <- calculate_gp_cdf(q = 2:4, scale = 0.5, shape = 0, loc = 1)
-# 
-# result
-# 
-# 
-# # example 3
-# 
-# result <- calculate_gp_cdf(q = 2:4, scale = 0.5, shape = -0.2, loc = 1)
-# 
-# result
+# example 1
+
+result <- calculate_gp_cdf(q = 2:4, scale = 0.5, shape = 0.8, threshold = 1)
+
+result
+
+
+# example 2
+
+result <- calculate_gp_cdf(q = 2:4, scale = 0.5, shape = 0, threshold = 1)
+
+result
+
+
+# example 3
+
+result <- calculate_gp_cdf(q = 2:4, scale = 0.5, shape = -0.2, threshold = 1)
+
+result
---
+++ src/calculate_gp_mixture_model_cdf.R
@@ -0,0 +1,126 @@
+source("./src/calculate_gp_cdf.R")
+
+calculate_gp_mixture_model_cdf <- function(q, 
+                                            thresholds, 
+                                            scales, 
+                                            shapes, 
+                                            weights, 
+                                            kind = c("geometric", "arithmetic")[1]){
+  # q: vector of observations
+  # weights: vector of weights
+  # thresholds, scales, shapes: vectors of threshold, scale and shape parameters of the considered gp distributions
+  # The vectors of parameters must have the same number of elements
+  # kind: indicates the type of gp mixture model. Possible values are "geometric" or "arithmetic"
+  
+  if (kind == "geometric"){
+    output <- sapply(q, function(q) {
+      S <- sapply(1:length(shapes), function(j) {
+        prob <- calculate_gp_cdf(q = q, 
+                                  threshold = thresholds[j], 
+                                  scale = scales[j], 
+                                  shape = shapes[j])
+        
+        out <- prob^(weights[j])
+        
+        out
+      })
+      
+      G <- prod(S)
+      
+      G
+    })
+  }
+  else if (kind == "arithmetic"){
+    output <- sapply(q, function(q) {
+      S <- sapply(1:length(shapes), function(j) {
+        prob <- calculate_gp_cdf(q = q, 
+                                  threshold = thresholds[j], 
+                                  scale = scales[j], 
+                                  shape = shapes[j])
+        
+        out <- prob*weights[j]
+        
+        out
+      })
+      
+      G <- sum(S)
+      
+      G
+    })
+  }
+  else{
+    stop("Please enter a correct value to the argument 'kind'. Possible values are 'geometric' or 'arithmetic'!")
+  }
+  
+  output
+}
+
+
+
+# # example 1
+# 
+# p <- 10
+# 
+# y <- runif(p)
+# weights <- y/sum(y)
+# 
+# shapes <- runif(n = p, min = -0.1, max = +0.1)
+# scales <- rexp(n = p)
+# thresholds <- rnorm(n = p)
+# 
+# results <- calculate_gp_mixture_model_cdf(q = 10, 
+#                                            thresholds, 
+#                                            scales, 
+#                                            shapes, 
+#                                            weights,
+#                                            kind = c("geometric", "arithmetic")[1])
+# 
+# results
+# 
+# 
+# results <- calculate_gp_mixture_model_cdf(q = 10, 
+#                                            thresholds, 
+#                                            scales, 
+#                                            shapes, 
+#                                            weights,
+#                                            kind = c("geometric", "arithmetic")[2])
+# 
+# results
+# 
+# 
+# results <- calculate_gp_mixture_model_cdf(q = 10, 
+#                                            thresholds, 
+#                                            scales, 
+#                                            shapes, 
+#                                            weights,
+#                                            kind = "geom")
+# 
+# 
+# # example 2
+# 
+# p <- 100
+# 
+# y <- runif(p)
+# weights <- y/sum(y)
+# 
+# shapes <- runif(n = p, min = -0.1, max = +0.1)
+# scales <- rexp(n = p)
+# thresholds <- rnorm(n = p)
+# 
+# results <- calculate_gp_mixture_model_cdf(q = c(2, 3, 5, 7, 11, 13, 17, 19), 
+#                                            thresholds, 
+#                                            scales, 
+#                                            shapes, 
+#                                            weights,
+#                                            kind = c("geometric", "arithmetic")[1])
+# 
+# results
+# 
+# results <- calculate_gp_mixture_model_cdf(q = c(2, 3, 5, 7, 11, 13, 17, 19), 
+#                                            thresholds, 
+#                                            scales, 
+#                                            shapes, 
+#                                            weights,
+#                                            kind = c("geometric", "arithmetic")[2])
+# 
+# results
