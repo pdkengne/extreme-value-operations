3621f1f062008b3fe3e4c4649246636025e452ac
pdkengne <sielenou_alain@yahoo.fr>
2023-12-11 10:47:39 +0100

    creation of the file calculate_laplace_mixture_model_inverse_cdf.R

---
+++ src/calculate_laplace_mixture_model_inverse_cdf.R
@@ -0,0 +1,162 @@
+source("./src/calculate_laplace_mixture_model_cdf.R")
+source("./src/dichotomy.R")
+
+calculate_laplace_mixture_model_inverse_cdf <- function(p, 
+                                                        locations, 
+                                                        scales,
+                                                        weights, 
+                                                        kind = c("geometric", "arithmetic")[1],
+                                                        iterations = 100){
+  # p: vector of probabilities
+  # weights: vector of weights
+  # locations, scales: vectors of location and scale parameters of the considered distributions
+  # The vectors of parameters must have the same number of elements
+  # iterations: number of iterations to perform in the the dichotomy algorithm
+  
+  if (is.element(el = kind, set = c("geometric", "arithmetic"))){
+    output <- sapply(p, function(p){
+      # define the nonlinear equation to solve
+      nle <- function(q){
+        f <- -p + calculate_laplace_mixture_model_cdf(q = q, 
+                                                      locations = locations, 
+                                                      scales = scales, 
+                                                      weights = weights,
+                                                      kind = kind)
+        f
+      }
+      
+      # get the positions where weights are different from zero
+      position_weights_nonzero <- which(weights > 0)
+      
+      # extract all parameters for which weights are different from zero
+      locations <- locations[position_weights_nonzero]
+      scales <- scales[position_weights_nonzero]
+      weights <- weights[position_weights_nonzero]
+      
+      # calculates some initial guesses for the root of the nonlinear equation to solve
+      q_initial_guesses <- sapply(1:length(weights), function(j) extraDistr::qlaplace(p = p, 
+                                                                                      mu = locations[j], 
+                                                                                      sigma = scales[j])) 
+      
+      # estimate the root of the nonlinear equation to solve
+      answer <- dichotomy(func = nle, a = min(q_initial_guesses), b = max(q_initial_guesses), n = iterations)
+      
+      answer
+    })
+  }
+  else{
+    stop("Please enter a correct value to the argument 'kind'. Possible values are 'geometric' or 'arithmetic'!")
+  }
+  
+  output
+}
+
+
+
+# example 1
+
+m <- 3
+
+weights <- as.numeric(extraDistr::rdirichlet(n = 1, alpha = rep(1, times = m)))
+
+scales <- rexp(n = m)
+locations <- rnorm(n = m)
+
+p <- seq(from = 0.01, to = 0.09, length.out = 9)
+
+results <- calculate_laplace_mixture_model_inverse_cdf(p = p,
+                                                       locations,
+                                                       scales,
+                                                       weights,
+                                                       iterations = 100,
+                                                       kind = c("geometric", "arithmetic")[1])
+
+results
+
+calculate_laplace_mixture_model_cdf(q = results,
+                                    locations,
+                                    scales,
+                                    weights,
+                                    kind = c("geometric", "arithmetic")[1])
+p
+
+
+results <- calculate_laplace_mixture_model_inverse_cdf(p = p,
+                                                       locations,
+                                                       scales,
+                                                       weights,
+                                                       iterations = 100,
+                                                       kind = c("geometric", "arithmetic")[2])
+
+results
+
+calculate_laplace_mixture_model_cdf(q = results,
+                                    locations,
+                                    scales,
+                                    weights,
+                                    kind = c("geometric", "arithmetic")[2])
+p
+
+
+results <- calculate_laplace_mixture_model_inverse_cdf(p = p,
+                                                       locations,
+                                                       scales,
+                                                       weights,
+                                                       iterations = 100,
+                                                       kind = "mix")
+
+
+
+# example 2
+
+m <- 3
+
+weights <- as.numeric(extraDistr::rdirichlet(n = 1, alpha = rep(1, times = m)))
+
+scales <- rexp(n = m)
+locations <- rnorm(n = m)
+
+p <- seq(from = 0.90, to = 0.99, length.out = 10)
+
+results <- calculate_laplace_mixture_model_inverse_cdf(p = p,
+                                                       locations,
+                                                       scales,
+                                                       weights,
+                                                       iterations = 100,
+                                                       kind = c("geometric", "arithmetic")[1])
+
+results
+
+calculate_laplace_mixture_model_cdf(q = results,
+                                    locations,
+                                    scales,
+                                    weights,
+                                    kind = c("geometric", "arithmetic")[1])
+p
+
+
+results <- calculate_laplace_mixture_model_inverse_cdf(p = p,
+                                                       locations,
+                                                       scales,
+                                                       weights,
+                                                       iterations = 100,
+                                                       kind = c("geometric", "arithmetic")[2])
+
+results
+
+calculate_laplace_mixture_model_cdf(q = results,
+                                    locations,
+                                    scales,
+                                    weights,
+                                    kind = c("geometric", "arithmetic")[2])
+p
+
+
+results <- calculate_laplace_mixture_model_inverse_cdf(p = p,
+                                                       locations,
+                                                       scales,
+                                                       weights,
+                                                       iterations = 100,
+                                                       kind = "mix")
+
+
