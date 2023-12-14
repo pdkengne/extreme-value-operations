9a3fdf4eaef28f2052f7855a330144f36ef33e08
pdkengne <sielenou_alain@yahoo.fr>
2023-12-14 14:44:45 +0100

    update the simulation procedure in the function generate_gev_mixture_model_sample

--- src/generate_gev_mixture_model_sample.R
+++ src/generate_gev_mixture_model_sample.R
@@ -1,5 +1,5 @@
 source("./src/calculate_gev_mixture_model_inverse_cdf.R")
-source("./src/calculate_power_gev_parameters.R")
+source("./src/calculate_gev_inverse_cdf.R")
 source("./src/generate_gev_sample.R")
 
 generate_gev_mixture_model_sample <- function(n = 1, 
@@ -34,15 +34,12 @@ generate_gev_mixture_model_sample <- function(n = 1,
   else if (kind == "geometric"){
     output <- sapply(1:n, function(j){
       out <- sapply(1:length(weights), function(k){
-        parameter_star <- calculate_power_gev_parameters(loc = locations[k], 
-                                                         scale = scales[k], 
-                                                         shape = shapes[k], 
-                                                         exponent = weights[k])
+        power_random_variates <- exp(-rexp(n = 1, rate = weights[k]))
         
-        random_value <- generate_gev_sample(n = 1, 
-                                            loc = parameter_star["loc_star"], 
-                                            scale = parameter_star["scale_star"], 
-                                            shape = parameter_star["shape_star"])
+        random_value <- calculate_gev_inverse_cdf(p = power_random_variates, 
+                                                  loc = locations[k], 
+                                                  scale = scales[k], 
+                                                  shape = shapes[k])
         
         random_value
       })
@@ -95,8 +92,8 @@ generate_gev_mixture_model_sample <- function(n = 1,
 # support_density_1 <- density_1$x
 # value_density_1 <- density_1$y
 # 
-# hist(x = results_1, 
-#      freq = FALSE, 
+# hist(x = results_1,
+#      freq = FALSE,
 #      ylim = range(c(value_density_1, pdf_1)),
 #      ylab = "density",
 #      xlab = "support",
@@ -131,8 +128,8 @@ generate_gev_mixture_model_sample <- function(n = 1,
 # support_density_2 <- density_2$x
 # value_density_2 <- density_2$y
 # 
-# hist(x = results_2, 
-#      freq = FALSE, 
+# hist(x = results_2,
+#      freq = FALSE,
 #      ylim = range(c(value_density_2, pdf_2)),
 #      ylab = "density",
 #      xlab = "support",
@@ -144,12 +141,12 @@ generate_gev_mixture_model_sample <- function(n = 1,
 # legend("topright", legend = c("empirical", "theoretical"), col = c(1, 4), lty = c(1, 1))
 # 
 # 
-# plot(x = sort(results_1), 
-#      y = pdf_1, 
-#      type = "l", 
+# plot(x = sort(results_1),
+#      y = pdf_1,
+#      type = "l",
 #      col = 7,
 #      lwd = 2,
-#      ylim = range(c(pdf_1, pdf_2)), 
+#      ylim = range(c(pdf_1, pdf_2)),
 #      xlim = range(c(results_1, results_2)),
 #      xlab = "support",
 #      ylab = "density",
@@ -205,8 +202,8 @@ generate_gev_mixture_model_sample <- function(n = 1,
 # support_density_1 <- density_1$x
 # value_density_1 <- density_1$y
 # 
-# hist(x = results_1, 
-#      freq = FALSE, 
+# hist(x = results_1,
+#      freq = FALSE,
 #      ylim = range(c(value_density_1, pdf_1)),
 #      ylab = "density",
 #      xlab = "support",
@@ -241,8 +238,8 @@ generate_gev_mixture_model_sample <- function(n = 1,
 # support_density_2 <- density_2$x
 # value_density_2 <- density_2$y
 # 
-# hist(x = results_2, 
-#      freq = FALSE, 
+# hist(x = results_2,
+#      freq = FALSE,
 #      ylim = range(c(value_density_2, pdf_2)),
 #      ylab = "density",
 #      xlab = "support",
@@ -254,12 +251,12 @@ generate_gev_mixture_model_sample <- function(n = 1,
 # legend("topright", legend = c("empirical", "theoretical"), col = c(1, 4), lty = c(1, 1))
 # 
 # 
-# plot(x = sort(results_1), 
-#      y = pdf_1, 
-#      type = "l", 
+# plot(x = sort(results_1),
+#      y = pdf_1,
+#      type = "l",
 #      col = 7,
 #      lwd = 2,
-#      ylim = range(c(pdf_1, pdf_2)), 
+#      ylim = range(c(pdf_1, pdf_2)),
 #      xlim = range(c(results_1, results_2)),
 #      xlab = "support",
 #      ylab = "density",
@@ -268,3 +265,4 @@ generate_gev_mixture_model_sample <- function(n = 1,
 # lines(x = sort(results_2), y  = pdf_2, type = "l", col = 6, lwd = 2)
 # 
 # legend("topright", legend = c("arithmetic", "geometric"), col = c(7, 6), lty = c(1, 1))
+
