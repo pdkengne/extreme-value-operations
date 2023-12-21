8ba4203d2e1efd92ed6383e55d0af02af999619f
pdkengne <sielenou_alain@yahoo.fr>
2023-12-21 15:46:15 +0100

    creation of the file calculate_stationary_gev_mixture_model_inverse_cdf.R

---
+++ src/calculate_stationary_gev_mixture_model_inverse_cdf.R
@@ -0,0 +1,78 @@
+source("./src/calculate_gev_mixture_model_inverse_cdf.R")
+
+calculate_stationary_gev_mixture_model_inverse_cdf <- function(gev_mixture_model_object,
+                                                               p,
+                                                               kind = c("geometric", "arithmetic")[1],
+                                                               iterations = 100){
+  # gev_mixture_model_object: an object associated with a result of the function "fit_stationary_gev_mixture_model()"
+  # kind: indicates the type of gev mixture model. Possible values are "geometric" or "arithmetic"
+  # p: vector of probabilities
+  
+  # get the normalized gev parameters
+  if (gev_mixture_model_object$use_extremal_index){
+    normalized_gev_parameters <- gev_mixture_model_object$full_normalized_gev_parameters_object
+  } 
+  else{
+    normalized_gev_parameters <- gev_mixture_model_object$normalized_gev_parameters_object
+  }
+  
+  shapes <- normalized_gev_parameters$shape_star
+  scales <- normalized_gev_parameters$scale_star
+  locations <- normalized_gev_parameters$loc_star
+  
+  # get the vector of weights
+  weights <- gev_mixture_model_object$weights
+  
+  # calculate the vector of inverse cdf
+  output <- calculate_gev_mixture_model_inverse_cdf(p = p, 
+                                                    locations = locations, 
+                                                    scales = scales, 
+                                                    shapes = shapes, 
+                                                    weights = weights,
+                                                    kind = kind,
+                                                    iterations = iterations)
+  
+  output
+}
+
+
+
+# # example 1
+# 
+# source("./src/fit_stationary_gev_mixture_model.R")
+# source("./src/generate_gev_sample.R")
+# 
+# n <- 3000
+# 
+# x <- rnorm(n = n)
+# 
+# #x <- rexp(n = n, rate = 1)
+# 
+# #x <- generate_gev_sample(n = n, loc = 1, scale = 0.5, shape = 0.01)
+# 
+# #x <- generate_gev_sample(n = n, loc = 1, scale = 0.5, shape = 0.01)
+# 
+# gev_mixture_model_object <- fit_stationary_gev_mixture_model(x = x,
+#                                                              block_sizes = NULL,
+#                                                              minimum_nblocks = 50,
+#                                                              threshold = NULL,
+#                                                              confidence_level = 0.95,
+#                                                              use_extremal_index = TRUE,
+#                                                              use_uniform_prior = TRUE,
+#                                                              method = c("MLE", "GMLE", "Lmoments")[1])
+# 
+# 
+# p <- seq(from = 0.90, to = 0.99, length.out = 10)
+# 
+# results_geometric <- calculate_stationary_gev_mixture_model_inverse_cdf(gev_mixture_model_object,
+#                                                                         p = p,
+#                                                                         kind = c("geometric", "arithmetic")[1])
+# 
+# results_geometric
+# 
+# results_arithmetic <- calculate_stationary_gev_mixture_model_inverse_cdf(gev_mixture_model_object,
+#                                                                          p = p,
+#                                                                          kind = c("geometric", "arithmetic")[2])
+# 
+# results_arithmetic
+
