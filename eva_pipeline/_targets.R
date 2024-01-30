63af24a9d3a33547c3c6ccfd4fedb64376285cc7
pdkengne <sielenou_alain@yahoo.fr>
2024-01-30 20:37:31 +0100

    creation of eva_pipeline diagram

---
+++ eva_pipeline/_targets.R
@@ -0,0 +1,186 @@
+# Written by use_targets().
+# Follow the comments below to fill in this target script.
+# Then follow the manual to check and run the pipeline:
+#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline # nolint
+
+# Load packages required to define the pipeline:
+library(targets)
+library(tarchetypes) # Load other packages as needed. # nolint
+
+read_data <- function(data_path, nrow_skip = 0, header = TRUE, sep = ",", dec = "."){
+  
+  print("read_data: ok")
+  
+}
+
+
+make_data_path <- function(main_dir, csv_file_name){
+  
+  print("make_data_path: ok")
+  
+}
+
+
+transform_data <- function(data, 
+                           response_var,
+                           response_abs = FALSE,
+                           scale_predictors = TRUE,
+                           coefficient_iqr = Inf, 
+                           remove_outliers = FALSE,
+                           method = c("interpolate", "mode", "median", "mean")[1]){
+  
+  print("transform_data: ok")
+  
+}
+
+
+make_models <- function(predictor_name = ~1){
+  
+  print("make_models: ok")
+  
+}
+
+
+calculate_models_aic <- function(transformed_data, models){
+  
+  print("calculate_model_aic: ok")
+  
+}
+
+
+
+make_aic_path <- function(main_dir, predictor_name = ~1){
+  
+  print("make_aic_path: ok")
+  
+}
+
+
+
+save_models_aic <- function(models_aic, aic_path){
+  
+  print("save_models_aic: ok")
+  
+}
+
+
+
+
+
+
+
+# Set target options:
+tar_option_set(
+  packages = c("dplyr", "ggplot2", "readr"), # packages that your targets need to run
+  format = "rds" # default storage format
+  # Set other options as needed.
+)
+
+# Configure the backend of tar_make_clustermq() (recommended):
+options(clustermq.scheduler = "multicore")
+
+# Configure the backend of tar_make_future() (optional):
+future::plan(future.callr::callr)
+
+# Load the R scripts with your custom functions:
+# lapply(list.files("R", full.names = TRUE), source)
+# source("other_functions.R") # Source other scripts as needed. # nolint
+
+
+
+# Replace the target list below with your own:
+list(
+  tar_target(name = main_dir, command = "main_dir", pattern = NULL),
+  
+  tar_target(name = csv_file_name, command = "file.csv", pattern = NULL),
+  
+  tar_target(name = nrow_skip, command = c(0), pattern = NULL),
+  
+  
+  tar_target(name = data_path, 
+             command = make_data_path(main_dir = main_dir, 
+                                      csv_file_name = csv_file_name), 
+             # format = "file",
+             pattern = NULL),
+  
+  
+  tar_target(name = data, 
+             command = read_data(data_path = data_path, 
+                                 nrow_skip = nrow_skip, 
+                                 header = TRUE, 
+                                 sep = ",", 
+                                 dec = "."),
+             pattern = NULL),
+  
+  
+  tar_target(name = response_var, command = "response_var_name", pattern = NULL),
+  
+  
+  tar_target(name = transformed_data, 
+             command = transform_data(data = data, 
+                                      response_var = response_var,
+                                      response_abs = TRUE,
+                                      scale_predictors = TRUE,
+                                      coefficient_iqr = 9, 
+                                      remove_outliers = FALSE,
+                                      method = c("interpolate", "mode", "median", "mean")[1]), 
+             pattern = NULL),
+  
+  
+  tar_target(name = predictor_name, command = ~var_name, pattern = NULL),
+  
+  
+  tar_target(name = models, 
+             command = make_models(predictor_name = predictor_name), 
+             pattern = NULL),
+  
+  
+  tar_target(name = models_aic, 
+             command = calculate_models_aic(transformed_data = transformed_data, 
+                                            models = models), 
+             pattern = NULL),
+  
+  
+  tar_target(name = aic_path, 
+             command = make_aic_path(main_dir = main_dir, 
+                                     predictor_name = predictor_name), 
+             pattern = NULL),
+  
+  
+  tar_target(name = saved_aic, 
+             command = save_models_aic(models_aic = models_aic, 
+                                       aic_path = aic_path), 
+             pattern = NULL)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  # tar_target(model, fit_model(data)),
+  # tar_target(plot, plot_model(model, data)),
+  # 
+  # tar_target(x, c(10, 20, 30)),
+  # tar_target(y, c(1, 2, 3)),
+  # tar_target(z, c(2, 4, 6)),
+  # tar_target(
+  #   resultat_1,
+  #   data.frame(argument_1 = x, argument_2 = y, res = simulation(x, y)),
+  #   pattern = map(x, y)),
+  
+  # tar_target(
+  #   resultat_2,
+  #   data.frame(argument_1 = x, argument_2 = y, res = simulation(x, y)),
+  #   pattern = cross(x, y)),
+  
+  # tar_target(
+  #   resultat_3,
+  #   data.frame(argument_1 = x, argument_2 = y, argument_3 = z, res = simulation(x, y, z)),
+  #   pattern = cross(x, map(y, z)))
+  
+)
---
+++ eva_pipeline/src/calculate_models_aic.R
@@ -0,0 +1,66 @@
+source("./eva_pipeline/src/load_functions.R")
+
+load_functions()
+
+calculate_model_aic <- function(transformed_data, models){
+  # transformed_data:
+  # models:
+  
+  use.phi <- TRUE
+  nlargest <- 20000
+  block_sizes <- NULL
+  minimum_nblocks <- 50
+  threshold <- min(x)
+  confidence_level <- 0.95
+  use_extremal_index <- TRUE
+  use_uniform_prior <- TRUE
+  method <- "MLE"
+  
+  x <- transformed_data$response
+  data <- transformed_data$predictors
+  
+  fitted_models_object <- lapply(models, function(model){
+    try({
+      ns_gev_mixture_model_object <- fit_non_stationary_gev_mixture_model(x = x,
+                                                                          data = data,
+                                                                          location.fun = model$location.fun,
+                                                                          scale.fun = model$scale.fun,
+                                                                          shape.fun = model$shape.fun,
+                                                                          use.phi = use.phi,
+                                                                          nlargest = nlargest,
+                                                                          block_sizes = block_sizes,
+                                                                          minimum_nblocks = minimum_nblocks,
+                                                                          threshold = threshold,
+                                                                          confidence_level = confidence_level,
+                                                                          use_extremal_index = use_extremal_index,
+                                                                          use_uniform_prior = use_uniform_prior,
+                                                                          method = method)
+      
+      model_vector <- as.character(model)
+      names(model_vector) <- c("location.fun", "scale.fun", "shape.fun")
+      
+      information_criteria <- ns_gev_mixture_model_object$information_criteria
+      
+      c(model_vector, information_criteria)
+    },
+    silent = TRUE)
+    
+  })
+  
+  success <- sapply(fitted_models_object, function(x) !inherits(x, "try-error"))
+  
+  fitted_models_object_success <- fitted_models_object[success]
+  
+  fitted_models_object_success_df <- do.call(rbind, fitted_models_object_success)
+  
+  fitted_models_object_success_df <- data.frame("model_names" = rownames(fitted_models_object_success_df),
+                                                fitted_models_object_success_df)
+  
+  rownames(fitted_models_object_success_df) <- 1:nrow(fitted_models_object_success_df)
+  
+  fitted_models_object_success_df
+  
+}
+
+
+
