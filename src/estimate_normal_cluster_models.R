810428d4b458403a7ff26fc8cb08f32296d65060
pdkengne <sielenou_alain@yahoo.fr>
2024-02-04 00:32:42 +0100

    creation of the file estimate_normal_cluster_models.R

--- src/estimate_normal_cluster_models.R
+++ src/estimate_normal_cluster_models.R
@@ -12,19 +12,19 @@ estimate_normal_cluster_models <- function(cluster_data){
 }
 
 
-# example 1
-
-n <- 100
-x <- bmixture::rmixnorm(n = n, weight = c(1/2, 1/2), mean = c(-10, +10), sd = c(1, 1))
-
-
-nclusters <- 3
-
-initial_cluster_data <- initialize_cluster_data(x = x, nclusters = 3)
-
-initial_cluster_data
-
-
-cluster_models <- estimate_normal_cluster_models(cluster_data = initial_cluster_data)
-
-cluster_models
+# # example 1
+# 
+# n <- 100
+# x <- bmixture::rmixnorm(n = n, weight = c(1/2, 1/2), mean = c(-10, +10), sd = c(1, 1))
+# 
+# 
+# nclusters <- 3
+# 
+# initial_cluster_data <- initialize_cluster_data(x = x, nclusters = 3)
+# 
+# initial_cluster_data
+# 
+# 
+# cluster_models <- estimate_normal_cluster_models(cluster_data = initial_cluster_data)
+# 
+# cluster_models
