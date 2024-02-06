860991e3c1fb37f4ee0ce2d21a879fab2bc2de10
pdkengne <58425766+pdkengne@users.noreply.github.com>
2024-02-06 02:49:08 +0100

    creation of the file plot_fit_stationary_weibull_mixture_model.R

--- src/plot_fit_stationary_weibull_mixture_model.R
+++ src/plot_fit_stationary_weibull_mixture_model.R
@@ -32,8 +32,8 @@ plot_fit_stationary_weibull_mixture_model <- function(weibull_mixture_model_obje
   
   cluster_models_parameters <- weibull_mixture_model_object$cluster_models_coefficients
   
-  locations <- cluster_models_parameters[, "mean"]
-  scales <- cluster_models_parameters[, "sd"]
+  locations <- cluster_models_parameters[, "shape"]
+  scales <- cluster_models_parameters[, "scale"]
   
   cluster_attractors_weights <- weibull_mixture_model_object$cluster_attractors_weights
   
@@ -80,110 +80,110 @@ plot_fit_stationary_weibull_mixture_model <- function(weibull_mixture_model_obje
 }
 
 
-# example 1
-
-source("./src/fit_stationary_weibull_mixture_model.R")
-
-library(mixR)
-
-set.seed(102)
-x = rmixweibull(1000, c(0.3, 0.7), c(-2, 3), c(2, 1))
-
-mod1 = mixfit(x, ncomp = 2, family = 'weibull')
-mod1
-
-weibull_mixture_model_object <- fit_stationary_weibull_mixture_model(x = x,
-                                                                   nclusters = 2,
-                                                                   centers = NULL,
-                                                                   minimum_cluster_size = 20,
-                                                                   prior_cluster_weights = NULL,
-                                                                   confidence_level = 0.95)
-
-names(weibull_mixture_model_object)
-
-# [1] "x"                              "cluster_data_list"              "cluster_models"
-# [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
-# [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
-# [10] "loglik"                         "cluster_information_criteria"
-
-weibull_mixture_model_object
-
-plot_fit_stationary_weibull_mixture_model(weibull_mixture_model_object = weibull_mixture_model_object,
-                                          xlab = "support",
-                                          ylab = "density",
-                                          main = "density plot",
-                                          legend_position = "topleft")
-
-
-# example 2
-
-source("./src/fit_stationary_weibull_mixture_model.R")
-
-library(mixtools)
-library(mixR)
-
-data(faithful)
-
-x <- faithful$waiting
-
-x <- faithful$eruptions
-
-mod1 = mixfit(x, ncomp = 2, family = 'weibull')
-mod1
-
-weibull_mixture_model_object <- fit_stationary_weibull_mixture_model(x = x,
-                                                                     nclusters = 2,
-                                                                     centers = NULL,
-                                                                     minimum_cluster_size = 20,
-                                                                     prior_cluster_weights = NULL,
-                                                                     confidence_level = 0.95)
-
-names(weibull_mixture_model_object)
-
-# [1] "x"                              "cluster_data_list"              "cluster_models"
-# [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
-# [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
-# [10] "loglik"                         "cluster_information_criteria"
-
-weibull_mixture_model_object
-
-plot_fit_stationary_weibull_mixture_model(weibull_mixture_model_object = weibull_mixture_model_object,
-                                          xlab = "support",
-                                          ylab = "density",
-                                          main = "density plot",
-                                          legend_position = "topleft")
-
-
-
-# example 3
-
-source("./src/fit_stationary_weibull_mixture_model.R")
-
-x <- mixR::rmixweibull(n = 2000, pi = c(2/4, 1/4, 1/4), mu = c(0.6, 1.3, 2.6), sd = c(0.1, 0.1, 0.1))
-
-mod1 = mixfit(x, ncomp = 3, family = 'weibull')
-mod1
-
-weibull_mixture_model_object <- fit_stationary_weibull_mixture_model(x = x,
-                                                                     nclusters = 3,
-                                                                     centers = NULL,
-                                                                     minimum_cluster_size = 20,
-                                                                     prior_cluster_weights = NULL,
-                                                                     confidence_level = 0.95)
-
-names(weibull_mixture_model_object)
-
-# [1] "x"                              "cluster_data_list"              "cluster_models"
-# [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
-# [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
-# [10] "loglik"                         "cluster_information_criteria"
-
-weibull_mixture_model_object
-
-plot_fit_stationary_weibull_mixture_model(weibull_mixture_model_object = weibull_mixture_model_object,
-                                          xlab = "support",
-                                          ylab = "density",
-                                          main = "density plot",
-                                          legend_position = "topleft")
+# # example 1
+# 
+# source("./src/fit_stationary_weibull_mixture_model.R")
+# 
+# library(mixR)
+# 
+# set.seed(102)
+# x = mixR::rmixweibull(1000, c(0.3, 0.7), c(0.6, 1.3), c(0.1, 0.1))
+# 
+# mod1 = mixfit(x, ncomp = 2, family = 'weibull')
+# mod1
+# 
+# weibull_mixture_model_object <- fit_stationary_weibull_mixture_model(x = x,
+#                                                                    nclusters = 2,
+#                                                                    centers = NULL,
+#                                                                    minimum_cluster_size = 20,
+#                                                                    prior_cluster_weights = NULL,
+#                                                                    confidence_level = 0.95)
+# 
+# names(weibull_mixture_model_object)
+# 
+# # [1] "x"                              "cluster_data_list"              "cluster_models"
+# # [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
+# # [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
+# # [10] "loglik"                         "cluster_information_criteria"
+# 
+# weibull_mixture_model_object
+# 
+# plot_fit_stationary_weibull_mixture_model(weibull_mixture_model_object = weibull_mixture_model_object,
+#                                           xlab = "support",
+#                                           ylab = "density",
+#                                           main = "density plot",
+#                                           legend_position = "topleft")
+# 
+# 
+# # example 2
+# 
+# source("./src/fit_stationary_weibull_mixture_model.R")
+# 
+# library(mixtools)
+# library(mixR)
+# 
+# data(faithful)
+# 
+# x <- faithful$waiting
+# 
+# x <- faithful$eruptions
+# 
+# mod1 = mixfit(x, ncomp = 2, family = 'weibull')
+# mod1
+# 
+# weibull_mixture_model_object <- fit_stationary_weibull_mixture_model(x = x,
+#                                                                      nclusters = 2,
+#                                                                      centers = NULL,
+#                                                                      minimum_cluster_size = 20,
+#                                                                      prior_cluster_weights = NULL,
+#                                                                      confidence_level = 0.95)
+# 
+# names(weibull_mixture_model_object)
+# 
+# # [1] "x"                              "cluster_data_list"              "cluster_models"
+# # [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
+# # [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
+# # [10] "loglik"                         "cluster_information_criteria"
+# 
+# weibull_mixture_model_object
+# 
+# plot_fit_stationary_weibull_mixture_model(weibull_mixture_model_object = weibull_mixture_model_object,
+#                                           xlab = "support",
+#                                           ylab = "density",
+#                                           main = "density plot",
+#                                           legend_position = "topleft")
+# 
+# 
+# 
+# # example 3
+# 
+# source("./src/fit_stationary_weibull_mixture_model.R")
+# 
+# x <- mixR::rmixweibull(n = 2000, pi = c(2/4, 1/4, 1/4), mu = c(0.6, 1.3, 2.6), sd = c(0.1, 0.1, 0.1))
+# 
+# mod1 = mixfit(x, ncomp = 3, family = 'weibull')
+# mod1
+# 
+# weibull_mixture_model_object <- fit_stationary_weibull_mixture_model(x = x,
+#                                                                      nclusters = 3,
+#                                                                      centers = NULL,
+#                                                                      minimum_cluster_size = 20,
+#                                                                      prior_cluster_weights = NULL,
+#                                                                      confidence_level = 0.95)
+# 
+# names(weibull_mixture_model_object)
+# 
+# # [1] "x"                              "cluster_data_list"              "cluster_models"
+# # [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
+# # [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
+# # [10] "loglik"                         "cluster_information_criteria"
+# 
+# weibull_mixture_model_object
+# 
+# plot_fit_stationary_weibull_mixture_model(weibull_mixture_model_object = weibull_mixture_model_object,
+#                                           xlab = "support",
+#                                           ylab = "density",
+#                                           main = "density plot",
+#                                           legend_position = "topleft")
 
 
