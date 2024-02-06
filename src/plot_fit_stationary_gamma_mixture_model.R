73ded7a9320cd7bd7f4a44da974f72e43bd399e5
pdkengne <58425766+pdkengne@users.noreply.github.com>
2024-02-06 16:42:40 +0100

    creation of the file plot_fit_stationary_gamma_mixture_model.R

--- src/plot_fit_stationary_gamma_mixture_model.R
+++ src/plot_fit_stationary_gamma_mixture_model.R
@@ -18,11 +18,13 @@ source("./src/calculate_gamma_mixture_model_inverse_cdf.R")
 
 
 plot_fit_stationary_gamma_mixture_model <- function(gamma_mixture_model_object,
-                                                     xlab = "support",
-                                                     ylab = "density",
-                                                     main = "density plot",
-                                                     legend_position = "topleft"){
+                                                    nclass = NULL,
+                                                    xlab = "support",
+                                                    ylab = "density",
+                                                    main = "density plot",
+                                                    legend_position = "topleft"){
   # gamma_mixture_model_object: an object associated with a result of the function "fit_stationary_gamma_mixture_model()"
+  # nclass:
   # xlab: label of the x-axis
   # ylab: label of the y-axis
   # main: title of the plot
@@ -48,21 +50,22 @@ plot_fit_stationary_gamma_mixture_model <- function(gamma_mixture_model_object,
                  length.out = 1000)
   
   density_geometric <- calculate_gamma_mixture_model_pdf(x = support, 
+                                                         shapes = shapes, 
+                                                         scales = scales, 
+                                                         weights = cluster_attractors_weights,
+                                                         kind = c("geometric", "arithmetic")[1])
+  
+  density_arithmetic <- calculate_gamma_mixture_model_pdf(x = support, 
                                                           shapes = shapes, 
                                                           scales = scales, 
                                                           weights = cluster_attractors_weights,
-                                                          kind = c("geometric", "arithmetic")[1])
-  
-  density_arithmetic <- calculate_gamma_mixture_model_pdf(x = support, 
-                                                           shapes = shapes, 
-                                                           scales = scales, 
-                                                           weights = cluster_attractors_weights,
-                                                           kind = c("geometric", "arithmetic")[2])
+                                                          kind = c("geometric", "arithmetic")[2])
   
   density_range <- range(c(density_empirical, density_geometric, density_arithmetic))
   
   hist(x = x, 
        probability = TRUE,
+       nclass = nclass,
        ylim = density_range, 
        xlim = range(support),
        xlab = xlab,
@@ -80,114 +83,119 @@ plot_fit_stationary_gamma_mixture_model <- function(gamma_mixture_model_object,
 }
 
 
-# example 1
-
-source("./src/fit_stationary_gamma_mixture_model.R")
-
-library(mixR)
-
-n <- 2000
-
-x <- bmixture::rmixgamma(n = n, weight = c(2/5, 3/5), alpha = c(9, 7), beta = c(0.5, 1))
-
-
-mod1 = mixfit(x, ncomp = 2, family = 'gamma')
-mod1
-
-gamma_mixture_model_object <- fit_stationary_gamma_mixture_model(x = x,
-                                                                   nclusters = 2,
-                                                                   centers = NULL,
-                                                                   minimum_cluster_size = 20,
-                                                                   prior_cluster_weights = NULL,
-                                                                   confidence_level = 0.95)
-
-names(gamma_mixture_model_object)
-
-# [1] "x"                              "cluster_data_list"              "cluster_models"
-# [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
-# [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
-# [10] "loglik"                         "cluster_information_criteria"
-
-gamma_mixture_model_object
-
-plot_fit_stationary_gamma_mixture_model(gamma_mixture_model_object = gamma_mixture_model_object,
-                                          xlab = "support",
-                                          ylab = "density",
-                                          main = "density plot",
-                                          legend_position = "topleft")
-
-
-# example 2
-
-source("./src/fit_stationary_gamma_mixture_model.R")
-
-library(mixtools)
-library(mixR)
-
-data(faithful)
-
-x <- faithful$waiting
-
-x <- faithful$eruptions
-
-mod1 = mixfit(x, ncomp = 2, family = 'gamma')
-mod1
-
-gamma_mixture_model_object <- fit_stationary_gamma_mixture_model(x = x,
-                                                                     nclusters = 2,
-                                                                     centers = NULL,
-                                                                     minimum_cluster_size = 20,
-                                                                     prior_cluster_weights = NULL,
-                                                                     confidence_level = 0.95)
-
-names(gamma_mixture_model_object)
-
-# [1] "x"                              "cluster_data_list"              "cluster_models"
-# [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
-# [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
-# [10] "loglik"                         "cluster_information_criteria"
-
-gamma_mixture_model_object
-
-plot_fit_stationary_gamma_mixture_model(gamma_mixture_model_object = gamma_mixture_model_object,
-                                          xlab = "support",
-                                          ylab = "density",
-                                          main = "density plot",
-                                          legend_position = "topleft")
-
-
-
-# example 3
-
-source("./src/fit_stationary_gamma_mixture_model.R")
-
-n <- 2000
-x <- bmixture::rmixgamma(n = n, weight = c(2/4, 1/4, 1/4), alpha = c(9, 7, 8), beta = c(0.5, 1, 2))
-
-
-mod1 = mixfit(x, ncomp = 3, family = 'gamma')
-mod1
-
-gamma_mixture_model_object <- fit_stationary_gamma_mixture_model(x = x,
-                                                                     nclusters = 3,
-                                                                     centers = NULL,
-                                                                     minimum_cluster_size = 20,
-                                                                     prior_cluster_weights = NULL,
-                                                                     confidence_level = 0.95)
-
-names(gamma_mixture_model_object)
-
-# [1] "x"                              "cluster_data_list"              "cluster_models"
-# [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
-# [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
-# [10] "loglik"                         "cluster_information_criteria"
-
-gamma_mixture_model_object
-
-plot_fit_stationary_gamma_mixture_model(gamma_mixture_model_object = gamma_mixture_model_object,
-                                          xlab = "support",
-                                          ylab = "density",
-                                          main = "density plot",
-                                          legend_position = "topleft")
+# # example 1
+# 
+# source("./src/fit_stationary_gamma_mixture_model.R")
+# 
+# library(mixR)
+# 
+# n <- 2000
+# 
+# x <- bmixture::rmixgamma(n = n, weight = c(2/5, 3/5), alpha = c(9, 7), beta = c(0.5, 1))
+# 
+# hist(x, nclass = 30)
+# 
+# mod1 = mixfit(x, ncomp = 2, family = 'gamma')
+# mod1
+# 
+# gamma_mixture_model_object <- fit_stationary_gamma_mixture_model(x = x,
+#                                                                  nclusters = 2,
+#                                                                  centers = NULL,
+#                                                                  minimum_cluster_size = 20,
+#                                                                  prior_cluster_weights = NULL,
+#                                                                  confidence_level = 0.95)
+# 
+# names(gamma_mixture_model_object)
+# 
+# # [1] "x"                              "cluster_data_list"              "cluster_models"
+# # [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
+# # [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
+# # [10] "loglik"                         "cluster_information_criteria"
+# 
+# gamma_mixture_model_object
+# 
+# plot_fit_stationary_gamma_mixture_model(gamma_mixture_model_object = gamma_mixture_model_object,
+#                                         nclass = 30,
+#                                         xlab = "support",
+#                                         ylab = "density",
+#                                         main = "density plot",
+#                                         legend_position = "topright")
+# 
+# 
+# # example 2
+# 
+# source("./src/fit_stationary_gamma_mixture_model.R")
+# 
+# library(mixtools)
+# library(mixR)
+# 
+# data(faithful)
+# 
+# x <- faithful$waiting
+# 
+# x <- faithful$eruptions
+# 
+# mod1 = mixfit(x, ncomp = 2, family = 'gamma')
+# mod1
+# 
+# gamma_mixture_model_object <- fit_stationary_gamma_mixture_model(x = x,
+#                                                                  nclusters = 2,
+#                                                                  centers = NULL,
+#                                                                  minimum_cluster_size = 20,
+#                                                                  prior_cluster_weights = NULL,
+#                                                                  confidence_level = 0.95)
+# 
+# names(gamma_mixture_model_object)
+# 
+# # [1] "x"                              "cluster_data_list"              "cluster_models"
+# # [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
+# # [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
+# # [10] "loglik"                         "cluster_information_criteria"
+# 
+# gamma_mixture_model_object
+# 
+# plot_fit_stationary_gamma_mixture_model(gamma_mixture_model_object = gamma_mixture_model_object,
+#                                         nclass = NULL,
+#                                         xlab = "support",
+#                                         ylab = "density",
+#                                         main = "density plot",
+#                                         legend_position = "topright")
+# 
+# 
+# 
+# # example 3
+# 
+# source("./src/fit_stationary_gamma_mixture_model.R")
+# 
+# n <- 2000
+# x <- bmixture::rmixgamma(n = n, weight = c(2/4, 1/4, 1/4), alpha = c(9, 7, 8), beta = c(0.5, 1, 2))
+# 
+# hist(x, nclass = 30)
+# 
+# mod1 = mixfit(x, ncomp = 3, family = 'gamma')
+# mod1
+# 
+# gamma_mixture_model_object <- fit_stationary_gamma_mixture_model(x = x,
+#                                                                  nclusters = 3,
+#                                                                  centers = 30,
+#                                                                  minimum_cluster_size = 20,
+#                                                                  prior_cluster_weights = NULL,
+#                                                                  confidence_level = 0.95)
+# 
+# names(gamma_mixture_model_object)
+# 
+# # [1] "x"                              "cluster_data_list"              "cluster_models"
+# # [4] "cluster_models_coefficients_ci" "iteration"                      "cluster_attractors_frequencies"
+# # [7] "cluster_attractors_weights"     "cluster_attractors_centers"     "cluster_models_coefficients"
+# # [10] "loglik"                         "cluster_information_criteria"
+# 
+# gamma_mixture_model_object
+# 
+# plot_fit_stationary_gamma_mixture_model(gamma_mixture_model_object = gamma_mixture_model_object,
+#                                         nclass = 30,
+#                                         xlab = "support",
+#                                         ylab = "density",
+#                                         main = "density plot",
+#                                         legend_position = "topright")
 
 
