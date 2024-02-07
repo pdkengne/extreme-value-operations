6487a1eba5145ae6aa8673080dc7a615e017e753
pdkengne <58425766+pdkengne@users.noreply.github.com>
2024-02-07 18:03:20 +0100

    creation of the file calculate_exp_cluster_attractors.R

--- src/calculate_exp_cluster_attractors.R
+++ src/calculate_exp_cluster_attractors.R
@@ -1,14 +1,14 @@
 source("./src/get_knn.R")
 source("./src/make_weights.R")
 source("./src/initialize_cluster_data.R")
-source("./src/calculate_normal_mixture_model_pdf.R")
+source("./src/calculate_gamma_mixture_model_pdf.R")
 
 
-calculate_normal_cluster_attractors <- function(x, 
-                                                cluster_models,
-                                                minimum_cluster_size = 20,
-                                                prior_cluster_weights = NULL,
-                                                confidence_level = 0.95){
+calculate_exp_cluster_attractors <- function(x, 
+                                             cluster_models,
+                                             minimum_cluster_size = 20,
+                                             prior_cluster_weights = NULL,
+                                             confidence_level = 0.95){
   # x:
   # cluster_models:
   # minimum_cluster_size:
@@ -34,9 +34,7 @@ calculate_normal_cluster_attractors <- function(x,
   
   cluster_attractors_matrix <- sapply(1:nclusters, function(k){
     parameters <- cluster_models_parameters[[k]]
-    densities <- dnorm(x = x, 
-                       mean = parameters["mean"], 
-                       sd = parameters["sd"])
+    densities <- dexp(x = x, rate = parameters["rate"])
     
     densities*prior_cluster_weights[k]
   })
@@ -76,15 +74,15 @@ calculate_normal_cluster_attractors <- function(x,
     model <- cluster_models[[k]]
     confint(object = model, level = confidence_level)
   })
-    
-  locations <- cluster_models_coefficients[, "mean"]
-  scales <- cluster_models_coefficients[, "sd"]
   
-  densities <- calculate_normal_mixture_model_pdf(x = x, 
-                                                  locations = locations, 
-                                                  scales = scales, 
-                                                  weights = cluster_attractors_weights,
-                                                  kind = c("geometric", "arithmetic")[2])
+  shapes <- rep(x = 1, times = nclusters)
+  scales <- 1/cluster_models_coefficients[, "rate"]
+  
+  densities <- calculate_gamma_mixture_model_pdf(x = x, 
+                                                 shapes = shapes, 
+                                                 scales = scales, 
+                                                 weights = cluster_attractors_weights,
+                                                 kind = c("geometric", "arithmetic")[2])
   
   loglik <- sum(log(densities))
   
@@ -107,7 +105,7 @@ calculate_normal_cluster_attractors <- function(x,
   
   cluster_data_list <- lapply(1:nclusters, function(k){
     center <- cluster_attractors_centers[k]
-
+    
     size <- ifelse(test = nclusters == 1, 
                    yes = ceiling(n - 1), 
                    no = ceiling(cluster_attractors_frequencies[k]))
@@ -139,30 +137,37 @@ calculate_normal_cluster_attractors <- function(x,
 # # example 1
 # 
 # source("./src/initialize_cluster_data.R")
-# source("./src/calculate_normal_mixture_model_cdf.R")
 # 
-# n <- 100
-# x <- bmixture::rmixnorm(n = n, weight = c(1/2, 1/2), mean = c(-10, +10), sd = c(1, 1))
+# n <- 1000
+# x <- bmixture::rmixgamma(n = n, weight = c(1/2, 1/2), alpha = c(9, 7), beta = c(0.5, 1))
+# 
+# hist(x, nclass = 30)
 # 
 # 
-# nclusters <- 3
+# nclusters <- 2
 # 
-# initial_cluster_data <- initialize_cluster_data(x = x, nclusters = 3)
+# initial_cluster_data <- initialize_cluster_data(x = x, nclusters = 2)
 # 
 # initial_cluster_data
 # 
 # 
-# cluster_models <- estimate_normal_cluster_models(cluster_data = initial_cluster_data)
+# cluster_models <- estimate_exp_cluster_models(cluster_data = initial_cluster_data)
 # 
 # cluster_models
 # 
-# prior_cluster_weights <- make_weights(positives_values = rep(1, times = nclusters))
+# prior_cluster_sizes <- sapply(initial_cluster_data, function(data){
+#   length(data)
+# })
+# 
+# prior_cluster_sizes
+# 
+# prior_cluster_weights <-  make_weights(positives_values = prior_cluster_sizes)
 # 
 # prior_cluster_weights
 # 
-# cluster_attractors <- calculate_normal_cluster_attractors(x = x, 
-#                                                           cluster_models = cluster_models, 
-#                                                           prior_cluster_weights = prior_cluster_weights)
+# cluster_attractors <- calculate_exp_cluster_attractors(x = x,
+#                                                        cluster_models = cluster_models,
+#                                                        prior_cluster_weights = prior_cluster_weights)
 # 
 # names(cluster_attractors)
 # 
