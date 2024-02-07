7bcfefb31f66eefd62195cb8b7cb599902255f44
pdkengne <58425766+pdkengne@users.noreply.github.com>
2024-02-07 12:23:38 +0100

    creation of the file calculate_evd_cluster_attractors.R

--- src/calculate_evd_cluster_attractors.R
+++ src/calculate_evd_cluster_attractors.R
@@ -1,14 +1,15 @@
 source("./src/get_knn.R")
 source("./src/make_weights.R")
 source("./src/initialize_cluster_data.R")
-source("./src/calculate_evd_mixture_model_pdf.R")
+source("./src/calculate_gev_pdf.R")
+source("./src/calculate_gev_mixture_model_pdf.R")
 
 
 calculate_evd_cluster_attractors <- function(x, 
-                                                 cluster_models,
-                                                 minimum_cluster_size = 20,
-                                                 prior_cluster_weights = NULL,
-                                                 confidence_level = 0.95){
+                                             cluster_models,
+                                             minimum_cluster_size = 20,
+                                             prior_cluster_weights = NULL,
+                                             confidence_level = 0.95){
   # x:
   # cluster_models:
   # minimum_cluster_size:
@@ -29,14 +30,15 @@ calculate_evd_cluster_attractors <- function(x,
   
   cluster_models_parameters <- lapply(1:nclusters, function(k){
     model <- cluster_models[[k]]
-    model$estimate
+    model$results$par
   })
   
   cluster_attractors_matrix <- sapply(1:nclusters, function(k){
     parameters <- cluster_models_parameters[[k]]
-    densities <- devd(x = x, 
-                          shape = parameters["shape"], 
-                          scale = parameters["scale"])
+    densities <- calculate_gev_pdf(x = x, 
+                                   shape = parameters["shape"], 
+                                   scale = parameters["scale"],
+                                   loc = parameters["location"])
     
     densities*prior_cluster_weights[k]
   })
@@ -67,24 +69,28 @@ calculate_evd_cluster_attractors <- function(x,
   
   cluster_models_coefficients <- lapply(selected_cluster_id, function(k){
     model <- cluster_models[[k]]
-    model$estimate
+    model$results$par
   })
   
   cluster_models_coefficients <- do.call(what = rbind, cluster_models_coefficients)
   
   cluster_models_coefficients_ci <- lapply(selected_cluster_id, function(k){
     model <- cluster_models[[k]]
-    confint(object = model, level = confidence_level)
+    extRemes::ci.fevd(x = model, 
+                      type = "parameter",
+                      alpha = 1 - confidence_level)
   })
-    
+  
   shapes <- cluster_models_coefficients[, "shape"]
   scales <- cluster_models_coefficients[, "scale"]
+  locations <- cluster_models_coefficients[, "location"]
   
-  densities <- calculate_evd_mixture_model_pdf(x = x, 
-                                                   shapes = shapes, 
-                                                   scales = scales, 
-                                                   weights = cluster_attractors_weights,
-                                                   kind = c("geometric", "arithmetic")[2])
+  densities <- calculate_gev_mixture_model_pdf(x = x, 
+                                               shapes = shapes, 
+                                               scales = scales,
+                                               locations = locations,
+                                               weights = cluster_attractors_weights,
+                                               kind = c("geometric", "arithmetic", "harmonic")[2])
   
   loglik <- sum(log(densities))
   
@@ -107,7 +113,7 @@ calculate_evd_cluster_attractors <- function(x,
   
   cluster_data_list <- lapply(1:nclusters, function(k){
     center <- cluster_attractors_centers[k]
-
+    
     size <- ifelse(test = nclusters == 1, 
                    yes = ceiling(n - 1), 
                    no = ceiling(cluster_attractors_frequencies[k]))
@@ -139,11 +145,11 @@ calculate_evd_cluster_attractors <- function(x,
 # # example 1
 # 
 # source("./src/initialize_cluster_data.R")
-# source("./src/calculate_evd_mixture_model_cdf.R")
 # 
 # n <- 1000
-# x <- mixR::rmixevd(n = n, pi = c(1/2, 1/2), mu = c(0.6, 1.3), sd = c(0.1, 0.1))
+# x <- bmixture::rmixnorm(n = n, weight = c(1/2, 1/2), mean = c(0.6, 1.3), sd = c(0.1, 0.1))
 # 
+# hist(x, nclass = NULL)
 # 
 # nclusters <- 2
 # 
@@ -161,8 +167,8 @@ calculate_evd_cluster_attractors <- function(x,
 # prior_cluster_weights
 # 
 # cluster_attractors <- calculate_evd_cluster_attractors(x = x,
-#                                                            cluster_models = cluster_models,
-#                                                            prior_cluster_weights = prior_cluster_weights)
+#                                                        cluster_models = cluster_models,
+#                                                        prior_cluster_weights = prior_cluster_weights)
 # 
 # names(cluster_attractors)
 # 
@@ -173,6 +179,50 @@ calculate_evd_cluster_attractors <- function(x,
 # cluster_attractors$cluster_attractors_weights
 # 
 # cluster_attractors$cluster_data_list
-
+# 
+# 
+# # example 2
+# 
+# source("./src/initialize_cluster_data.R")
+# source("./src/generate_gev_mixture_model_sample.R")
+# 
+# n <- 1000
+# x <- generate_gev_mixture_model_sample(n = n,
+#                                        weights = c(1/2, 1/2),
+#                                        locations = c(-3, +3),
+#                                        scales = c(1, 1),
+#                                        shapes = c(-0.01, +0.01),
+#                                        kind = c("geometric", "arithmetic", "harmonic")[2])
+# 
+# hist(x, nclass = 30)
+# 
+# nclusters <- 2
+# 
+# initial_cluster_data <- initialize_cluster_data(x = x, nclusters = 2)
+# 
+# initial_cluster_data
+# 
+# 
+# cluster_models <- estimate_evd_cluster_models(cluster_data = initial_cluster_data)
+# 
+# cluster_models
+# 
+# prior_cluster_weights <- make_weights(positives_values = rep(1, times = nclusters))
+# 
+# prior_cluster_weights
+# 
+# cluster_attractors <- calculate_evd_cluster_attractors(x = x,
+#                                                        cluster_models = cluster_models,
+#                                                        prior_cluster_weights = prior_cluster_weights)
+# 
+# names(cluster_attractors)
+# 
+# cluster_attractors$cluster_attractors_centers
+# 
+# cluster_attractors$cluster_attractors_frequencies
+# 
+# cluster_attractors$cluster_attractors_weights
+# 
+# cluster_attractors$cluster_data_list
 
 
